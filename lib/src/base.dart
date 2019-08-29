533d78ce963c3d5f1d430aa593c92aa8b6cd0f2d
matrixbirds <yooobuntu@gmail.com>
2019-08-29 21:34:46 +0800

    feat. upgrade ios native & flutter plugin

---
+++ lib/src/base.dart
@@ -0,0 +1,502 @@
+import 'dart:ui';
+
+class AgoraImage {
+  String url;
+  int x;
+  int y;
+  int width;
+  int height;
+
+  AgoraImage(this.url, this.x, this.y, this.width, this.height);
+
+  Map<String, dynamic> toJson() => {
+    'x': x,
+    'y': y,
+    'width': width,
+    'height': height,
+  };
+
+  @override
+  String toString() {
+    return "{x: $x, y: $y, width: $width, height: $height}";
+  }
+}
+class AgoraLiveTranscodingUser {
+  int uid;
+  int width;
+  int height;
+  int x;
+  int y;
+  int zOrder;
+  int alpha;
+  int audioChannel;
+
+  AgoraLiveTranscodingUser.fromJson(Map<dynamic, dynamic> json)
+    : uid = json['uid'],
+      width = json['width'],
+      height = json['height'],
+      x = json['x'],
+      y = json['y'],
+      zOrder = json['zOrder'],
+      alpha = json['alpha'],
+      audioChannel = json['audioChannel'];
+
+  Map<dynamic, dynamic> toJson() => {
+    'uid': uid,
+    'width': width,
+    'height': height,
+    'x': x,
+    'y': y,
+    'zOrder': zOrder,
+    'alpha': alpha,
+    'audioChannel': audioChannel
+  };
+}
+
+enum AgoraAudioCodecProfileType {
+  LCAAC,
+  HEAAC
+}
+
+Map<AgoraAudioCodecProfileType, int> _resolveAudioCodecProfileType = {
+  AgoraAudioCodecProfileType.LCAAC: 0,
+  AgoraAudioCodecProfileType.HEAAC: 1,
+};
+
+enum AgoraVideoCodecProfileType {
+  BaseLine,
+  Main,
+  High
+}
+
+Map<AgoraVideoCodecProfileType, int> _resolveVideoCodecProfileType = {
+  AgoraVideoCodecProfileType.BaseLine: 66,
+  AgoraVideoCodecProfileType.Main: 77,
+  AgoraVideoCodecProfileType.High: 100
+};
+
+enum AgoraAudioSampleRateType {
+  LowRateType,
+  MidRateType,
+  HighRateType
+}
+
+Map<AgoraAudioSampleRateType, int> _resolveAudioSampleRate = {
+  AgoraAudioSampleRateType.LowRateType: 32000,
+  AgoraAudioSampleRateType.MidRateType: 44100,
+  AgoraAudioSampleRateType.HighRateType: 48000
+};
+
+class AgoraLiveTranscoding {
+  int width;
+  int height;
+  int videoBitrate;
+  int videoFramerate;
+  bool lowLatency;
+  int videoGop;
+  AgoraVideoCodecProfileType videoCodecProfile;
+  List<AgoraLiveTranscodingUser> transcodingUsers;
+  String transcodingExtraInfo;
+  AgoraImage watermark;
+  AgoraImage backgroundImage;
+  String backgroundColor;
+  AgoraAudioSampleRateType audioSampleRate;
+  int audioBitrate;
+  int audioChannels;
+  AgoraAudioCodecProfileType audioCodecProfile;
+
+  AgoraLiveTranscoding.fromJson(Map<dynamic, dynamic> json)
+    : width = json['width'],
+      height = json['height'],
+      videoBitrate = json['videoBitrate'],
+      videoFramerate = json['videoFramerate'],
+      lowLatency = json['lowLatency'],
+      videoGop = json['videoGop'],
+      videoCodecProfile = json['videoCodecProfile'],
+      transcodingUsers = json['transcodingUsers'],
+      transcodingExtraInfo = json['transcodingExtraInfo'],
+      watermark = json['watermark'],
+      backgroundImage = json['backgroundImage'],
+      backgroundColor = json['backgroundColor'],
+      audioSampleRate = json['audioSampleRate'],
+      audioBitrate = json['audioBitrate'],
+      audioChannels = json['audioChannels'],
+      audioCodecProfile = json['audioCodecProfile'];
+
+
+  Map<String, dynamic> toJson() => {
+    'width': width,
+    'height': height,
+    'videoBitrate': videoBitrate,
+    'videoFramerate': videoFramerate,
+    'lowLatency': lowLatency,
+    'videoGop': videoGop,
+    'videoCodecProfile': _resolveVideoCodecProfileType[videoCodecProfile],
+    'transcodingUsers': transcodingUsers.map((item) => item.toJson()).toList(),
+    'transcodingExtraInfo': transcodingExtraInfo,
+    'watermark': watermark.toJson(),
+    'backgroundImage': backgroundImage.toJson(),
+    'audioSampleRate': _resolveAudioCodecProfileType[audioSampleRate],
+    'audioBitrate': audioBitrate,
+    'audioChannels': audioChannels,
+    'audioCodecProfile': _resolveAudioCodecProfileType[audioCodecProfile],
+  };
+}
+
+class AgoraLiveInjectStreamConfig {
+  int width;
+  int height;
+  int videoGop;
+  int videoFramerate;
+  int videoBitrate;
+  int audioBitrate;
+  AgoraAudioSampleRateType audioSampleRate;
+
+  AgoraLiveInjectStreamConfig.fromJson(Map<dynamic, dynamic> json)
+    : width = json['width'],
+      height = json['height'],
+      videoGop = json['videoGop'],
+      videoFramerate = json['videoFramerate'],
+      videoBitrate = json['videoBitrate'],
+      audioBitrate = json['audioBitrate'],
+      audioSampleRate = json['audioSampleRate'];
+
+  Map<String, dynamic> toJson() => {
+    'width': width,
+    'height': height,
+    'videoGop': videoGop,
+    'videoFramerate': videoFramerate,
+    'videoBitrate': videoBitrate,
+    'audioBitrate': audioBitrate,
+    'audioSampleRate': _resolveAudioSampleRate[audioSampleRate],
+  };
+
+  @override
+  String toString() {
+    return "{width: $width, height: $height, videoGop: $videoGop, videoFramerate: $videoFramerate, videoBitrate: $videoBitrate, audioBitrate: $audioBitrate, audioSampleRate: ${_resolveAudioSampleRate[audioSampleRate]}}";
+  }
+}
+
+class AudioVolumeInfo {
+  int uid;
+  int volume;
+
+  AudioVolumeInfo(int uid, int volume) {
+    this.uid = uid;
+    this.volume = volume;
+  }
+}
+
+class RtcStats {
+  int totalDuration;
+  int txBytes;
+  int rxBytes;
+  int txAudioKBitRate;
+  int rxAudioKBitRate;
+  int txVideoKBitRate;
+  int rxVideoKBitRate;
+  int users;
+  int lastmileDelay;
+  int txPacketLossRate;
+  int rxPacketLossRate;
+  double cpuTotalUsage;
+  double cpuAppUsage;
+}
+
+class LocalVideoStats {
+  int sentBitrate;
+  int sentFrameRate;
+  int encoderOutputFrameRate;
+  int rendererOutputFrameRate;
+}
+
+class RemoteVideoStats {
+  int uid;
+  int width;
+  int height;
+  int receivedBitrate;
+  int decoderOutputFrameRate;
+  int rendererOutputFrameRate;
+  int rxStreamType;
+}
+
+class RemoteAudioStats {
+  int uid;
+  int quality;
+  int networkTransportDelay;
+  int jitterBufferDelay;
+  int audioLossRate;
+}
+
+/// The image enhancement options in [AgoraRtcEngine.setBeautyEffectOptions].
+class BeautyOptions {
+  /// The lightening contrast level.
+  ///
+  /// 0: low contrast level.
+  /// 1: (default) normal contrast level.
+  /// 2: high contrast level.
+  LighteningContrastLevel lighteningContrastLevel =
+      LighteningContrastLevel.Normal;
+
+  /// The brightness level.
+  ///
+  /// The value ranges from 0.0 (original) to 1.0.
+  double lighteningLevel = 0;
+
+  /// The sharpness level.
+  ///
+  ///The value ranges from 0.0 (original) to 1.0. This parameter is usually used to remove blemishes.
+  double smoothnessLevel = 0;
+
+  /// The redness level.
+  ///
+  /// The value ranges from 0.0 (original) to 1.0. This parameter adjusts the red saturation level.
+  double rednessLevel = 0;
+
+  Map<String, double> jsonMap() {
+    return {
+      "lighteningContrastLevel": lighteningContrastLevel.index.toDouble(),
+      "lighteningLevel": lighteningLevel,
+      "smoothnessLevel": smoothnessLevel,
+      "rednessLevel": rednessLevel,
+    };
+  }
+}
+
+/// Properties of the video encoder configuration.
+class VideoEncoderConfiguration {
+  /// The video frame dimension used to specify the video quality in the total number of pixels along a frame's width and height.
+  ///
+  /// The dimension does not specify the orientation mode of the output ratio. For how to set the video orientation, see [VideoOutputOrientationMode].
+  /// Whether 720p can be supported depends on the device. If the device cannot support 720p, the frame rate will be lower than the one listed in the table. Agora optimizes the video in lower-end devices.
+  Size dimensions = Size(640, 360);
+
+  /// The frame rate of the video (fps).
+  ///
+  /// We do not recommend setting this to a value greater than 30.
+  int frameRate = 15;
+
+  /// The minimum video encoder frame rate (fps).
+  ///
+  /// The default value (-1) means the SDK uses the lowest encoder frame rate.
+  int minFrameRate = -1;
+
+  /// The bitrate of the video.
+  ///
+  /// Sets the video bitrate (Kbps). If you set a bitrate beyond the proper range, the SDK automatically adjusts it to a value within the range. You can also choose from the following options:
+  ///  - Standard: (recommended) In this mode, the bitrates differ between the Live-broadcast and Communication profiles:
+  ///   - Communication profile: the video bitrate is the same as the base bitrate.
+  ///   - Live-broadcast profile: the video bitrate is twice the base bitrate.
+  ///  - Compatible: In this mode, the bitrate stays the same regardless of the profile. In the Live-broadcast profile, if you choose this mode, the video frame rate may be lower than the set value.
+  /// Agora uses different video codecs for different profiles to optimize the user experience. For example, the Communication profile prioritizes the smoothness while the Live-broadcast profile prioritizes the video quality (a higher bitrate). Therefore, Agora recommends setting this parameter as AgoraVideoBitrateStandard.
+  int bitrate = AgoraVideoBitrateStandard;
+
+  /// The minimum encoding bitrate.
+  ///
+  /// The Agora SDK automatically adjusts the encoding bitrate to adapt to network conditions.
+  /// Using a value greater than the default value forces the video encoder to output high-quality images but may cause more packet loss and hence sacrifice the smoothness of the video transmission.
+  /// Unless you have special requirements for image quality, Agora does not recommend changing this value.
+  int minBitrate = -1;
+
+  /// The video orientation mode of the video.
+  VideoOutputOrientationMode orientationMode =
+      VideoOutputOrientationMode.Adaptative;
+
+  /// The video encoding degradation preference under limited bandwidth.
+  DegradationPreference degradationPreference =
+      DegradationPreference.MaintainQuality;
+
+  Map<String, dynamic> jsonMap() {
+    return {
+      'width': dimensions.width.toInt(),
+      'height': dimensions.height.toInt(),
+      'frameRate': frameRate,
+      'minFrameRate': minFrameRate,
+      'bitrate': bitrate,
+      'minBitrate': minBitrate,
+      'orientationMode': orientationMode.index,
+      'degradationPreference': degradationPreference.index,
+    };
+  }
+}
+
+const int AgoraVideoBitrateStandard = 0;
+const int AgoraVideoBitrateCompatible = -1;
+
+enum ChannelProfile {
+  /// This is used in one-on-one or group calls, where all users in the channel can talk freely.
+  Communication,
+
+  /// Host and audience roles that can be set by calling the [AgoraRtcEngine.setClientRole] method. The host sends and receives voice/video, while the audience can only receive voice/video.
+  LiveBroadcasting,
+}
+
+enum ClientRole {
+  Broadcaster,
+  Audience,
+}
+
+enum VideoOutputOrientationMode {
+  /// Adaptive mode.
+  ///
+  /// The video encoder adapts to the orientation mode of the video input device. When you use a custom video source, the output video from the encoder inherits the orientation of the original video.
+  /// If the width of the captured video from the SDK is greater than the height, the encoder sends the video in landscape mode. The encoder also sends the rotational information of the video, and the receiver uses the rotational information to rotate the received video.
+  /// If the original video is in portrait mode, the output video from the encoder is also in portrait mode. The encoder also sends the rotational information of the video to the receiver.
+  Adaptative,
+
+  /// Landscape mode.
+  ///
+  /// The video encoder always sends the video in landscape mode. The video encoder rotates the original video before sending it and the rotational information is 0. This mode applies to scenarios involving CDN live streaming.
+  FixedLandscape,
+
+  /// Portrait mode.
+  ///
+  /// The video encoder always sends the video in portrait mode. The video encoder rotates the original video before sending it and the rotational information is 0. This mode applies to scenarios involving CDN live streaming.
+  FixedPortrait,
+}
+
+/// The video encoding degradation preference under limited bandwidth.
+enum DegradationPreference {
+  /// Degrades the frame rate to guarantee the video quality.
+  MaintainQuality,
+
+  /// Degrades the video quality to guarantee the frame rate.
+  MaintainFramerate,
+
+  /// Reserved for future use.
+  Balanced,
+}
+
+enum VideoRenderMode {
+  /// Uniformly scale the video until it fills the visible boundaries (cropped). One dimension of the video may have clipped contents.
+  Hidden,
+
+  /// Uniformly scale the video until one of its dimension fits the boundary (zoomed to fit). Areas that are not filled due to the disparity in the aspect ratio are filled with black.
+  Fit,
+}
+
+enum VoiceChanger {
+  /// The original voice (no local voice change).
+  VOICE_CHANGER_OFF,
+
+  /// An old man's voice.
+  VOICE_CHANGER_OLDMAN,
+
+  /// A little boy's voice.
+  VOICE_CHANGER_BABYBOY,
+
+  ///A little girl's voice.
+  VOICE_CHANGER_BABYGILR,
+
+  /// Zhu Bajie's voice (Zhu Bajie is a character from Journey to the West who has a voice like a growling bear).
+  VOICE_CHANGER_ZHUBAJIE,
+
+  /// Ethereal vocal effects.
+  VOICE_CHANGER_ETHEREAL,
+
+  /// Hulk's voice.
+  VOICE_CHANGER_HULK
+}
+
+enum UserPriority {
+  High,
+  Normal,
+}
+
+enum StreamFallbackOptions {
+  /// No fallback behavior for the local/remote stream when the uplink/downlink network condition is unreliable. The quality of the stream is not guaranteed.
+  Disabled,
+
+  /// Under unreliable downlink network conditions, the remote stream falls back to the low-video stream (low resolution and low bitrate). You can only set this option in [AgoraRtcEngine.setRemoteSubscribeFallbackOption].
+  /// Nothing happens when you set this in [AgoraRtcEngine.setLocalPublishFallbackOption].
+  VideoStreamLow,
+
+  /// Under unreliable uplink network conditions, the published stream falls back audio only.
+  /// Under unreliable downlink network conditions, the remote stream first falls back to the low-video stream (low resolution and low bitrate); and then to an audio-only stream if the network condition deteriorates.
+  AudioOnly,
+}
+
+enum AudioProfile {
+  /// Default audio profile. In the communication profile, the default value is [SpeechStandard]; in the live-broadcast profile, the default value is [MusicStandard].
+  Default,
+
+  /// Sampling rate of 32 kHz, audio encoding, mono, and a bitrate of up to 18 Kbps.
+  SpeechStandard,
+
+  /// Sampling rate of 48 kHz, music encoding, mono, and a bitrate of up to 48 Kbps.
+  MusicStandard,
+
+  /// Sampling rate of 48 kHz, music encoding, stereo, and a bitrate of up to 56 Kbps.
+  MusicStandardStereo,
+
+  /// Sampling rate of 48 kHz, music encoding, mono, and a bitrate of up to 128 Kbps.
+  MusicHighQuality,
+
+  /// Sampling rate of 48 kHz, music encoding, stereo, and a bitrate of up to 192 Kbps.
+  MusicHighQualityStereo,
+}
+
+enum AudioScenario {
+  /// Default.
+  Default,
+
+  /// Entertainment scenario, supporting voice during gameplay.
+  ChatRoomEntertainment,
+
+  /// Education scenario, prioritizing fluency and stability.
+  Education,
+
+  /// Live gaming scenario, enabling the gaming audio effects in the speaker mode in a live broadcast scenario. Choose this scenario for high-fidelity music playback.
+  GameStreaming,
+
+  /// Showroom scenario, optimizing the audio quality with external professional equipment.
+  ShowRoom,
+
+  /// Gaming scenario.
+  ChatRoomGaming,
+}
+
+enum LighteningContrastLevel {
+  /// Low contrast level.
+  Low,
+
+  /// Normal contrast level.
+  Normal,
+
+  ///High contrast level.
+  High,
+}
+
+enum LocalVideoStreamState {
+  /// The local video is in the initial state.
+  Stopped,
+
+  /// The local video capturer starts successfully.
+  Capturing,
+
+  /// The first local video frame encodes successfully.
+  Encoding,
+
+  /// The local video fails to start.
+  Failed,
+}
+
+enum LocalVideoStreamError {
+  /// The local video is normal.
+  OK,
+
+  /// No specified reason for the local video failure.
+  Failure,
+
+  /// No permission to use the local video device.
+  DeviceNoPermission,
+
+  /// The local video capturer is in use.
+  DeviceBusy,
+
+  /// The local video capture fails. Check whether the capturer is working properly.
+  CaptureFailure,
+
+  /// The local video encoding fails.
+  EncodeFailure,
+}
