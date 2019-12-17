6a07c6955f8a6f6e262e395d334315efcea13b17
HUI <15215604969@163.com>
2019-12-17 14:23:44 +0800

    CSD-12543：fix orientation bug

--- android/src/main/java/io/agora/agorartcengine/AgoraRenderViewFactory.java
+++ android/src/main/java/io/agora/agorartcengine/AgoraRenderViewFactory.java
@@ -2,7 +2,6 @@ package io.agora.agorartcengine;
 
 import android.content.Context;
 import android.view.SurfaceView;
-import android.view.View;
 
 import io.flutter.plugin.common.MessageCodec;
 import io.flutter.plugin.platform.PlatformView;
@@ -20,7 +19,7 @@ public class AgoraRenderViewFactory extends PlatformViewFactory {
 
   @Override
   public PlatformView create(Context context, int id, Object o) {
-    SurfaceView view = RtcEngine.CreateRendererView(context);
+    SurfaceView view = RtcEngine.CreateRendererView(context.getApplicationContext());
     AgoraRendererView rendererView = new AgoraRendererView(view, id);
     mEnginePlugin.addView(view, id);
     return rendererView;
