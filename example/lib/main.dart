1a4833eadd97d48149bcac04549bc7debbaf5e36
HUI <luxuhui@agora.io>
2020-10-15 20:21:19 +0800

    feat: iOS raw data plugin

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -4,6 +4,7 @@ import 'package:agora_rtc_engine/rtc_engine.dart';
 import 'package:agora_rtc_engine/rtc_local_view.dart' as RtcLocalView;
 import 'package:agora_rtc_engine/rtc_remote_view.dart' as RtcRemoteView;
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 void main() {
@@ -16,6 +17,7 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  static const MethodChannel methodChannel = MethodChannel('io.agora.raw_data');
   bool _joined = false;
   int _remoteUid = null;
   bool _switch = false;
@@ -32,13 +34,15 @@ class _MyAppState extends State<MyApp> {
         .request();
 
     var engine = await RtcEngine.create(YOUR_APP_ID);
+    await methodChannel.invokeMethod(
+        'enableRawDataObserver', await engine.getNativeHandle());
     engine.setEventHandler(RtcEngineEventHandler(
-        joinChannelSuccess: (String channel, int uid, int elapsed) {
-          print('joinChannelSuccess ${channel} ${uid}');
-          setState(() {
-            _joined = true;
-          });
-        }, userJoined: (int uid, int elapsed) {
+        joinChannelSuccess: (String channel, int uid, int elapsed) async {
+      print('joinChannelSuccess ${channel} ${uid}');
+      setState(() {
+        _joined = true;
+      });
+    }, userJoined: (int uid, int elapsed) {
       print('userJoined ${uid}');
       setState(() {
         _remoteUid = uid;
@@ -79,7 +83,7 @@ class _MyAppState extends State<MyApp> {
                   },
                   child: Center(
                     child:
-                    _switch ? _renderLocalPreview() : _renderRemoteVideo(),
+                        _switch ? _renderLocalPreview() : _renderRemoteVideo(),
                   ),
                 ),
               ),
