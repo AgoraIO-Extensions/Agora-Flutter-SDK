fcb57cc3e2cbb3e16750e9ce8ddca069184ac1c0
HUI <luxuhui@agora.io>
2020-12-02 14:59:38 +0800

    feat: upgrade to 3.2.0

--- MediaObserver.kt
+++ MediaObserver.kt
@@ -6,35 +6,35 @@ import java.util.*
 import java.util.concurrent.atomic.AtomicInteger
 
 class MediaObserver(
-        private val emit: (data: Map<String, Any?>?) -> Unit
+  private val emit: (data: Map<String, Any?>?) -> Unit
 ) : IMetadataObserver {
-    private var maxMetadataSize = AtomicInteger(0)
-    private var metadataList = Collections.synchronizedList<String>(mutableListOf())
+  private var maxMetadataSize = AtomicInteger(0)
+  private var metadataList = Collections.synchronizedList<String>(mutableListOf())
 
-    fun addMetadata(metadata: String) {
-        metadataList.add(metadata)
-    }
+  fun addMetadata(metadata: String) {
+    metadataList.add(metadata)
+  }
 
-    fun setMaxMetadataSize(@IntRange(from = 0, to = 1024) size: Int) {
-        maxMetadataSize.set(size)
-    }
+  fun setMaxMetadataSize(@IntRange(from = 0, to = 1024) size: Int) {
+    maxMetadataSize.set(size)
+  }
 
-    override fun onReadyToSendMetadata(timeStampMs: Long): ByteArray? {
-        if (metadataList.size > 0) {
-            return metadataList.removeAt(0).toByteArray()
-        }
-        return null
+  override fun onReadyToSendMetadata(timeStampMs: Long): ByteArray? {
+    if (metadataList.size > 0) {
+      return metadataList.removeAt(0).toByteArray()
     }
+    return null
+  }
 
-    override fun getMaxMetadataSize(): Int {
-        return maxMetadataSize.get()
-    }
+  override fun getMaxMetadataSize(): Int {
+    return maxMetadataSize.get()
+  }
 
-    override fun onMetadataReceived(buffer: ByteArray, uid: Int, timeStampMs: Long) {
-        emit(hashMapOf(
-                "buffer" to String(buffer),
-                "uid" to uid,
-                "timeStampMs" to timeStampMs
-        ))
-    }
+  override fun onMetadataReceived(buffer: ByteArray, uid: Int, timeStampMs: Long) {
+    emit(hashMapOf(
+      "buffer" to String(buffer),
+      "uid" to uid,
+      "timeStampMs" to timeStampMs
+    ))
+  }
 }
