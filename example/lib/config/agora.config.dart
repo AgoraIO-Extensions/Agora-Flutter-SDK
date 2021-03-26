283fd175f17b47141be28eb4c3e8fdd3c8718eb8
HUI <luxuhui@agora.io>
2021-03-26 14:45:42 +0800

    fix: add config file

---
+++ example/lib/config/agora.config.dart
@@ -0,0 +1,14 @@
+/// Get your own App ID at https://dashboard.agora.io/
+const appId = YOUR_APP_ID;
+
+/// Please refer to https://docs.agora.io/en/Agora%20Platform/token
+const token = YOUR_TOEKN;
+
+/// Your channel ID
+const channelId = YOUR_CHANNEL_ID;
+
+/// Your int user ID
+const uid = YOUR_UID;
+
+/// Your string user ID
+const stringUid = YOUR_STRING_UID;
