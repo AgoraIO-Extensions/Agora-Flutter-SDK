2e2eb65300efcc39cd73677a9cc6cef38fc4c2df
HUI <luxuhui@agora.io>
2021-03-26 14:54:12 +0800

    Squashed 'android/src/main/java/io/agora/rtc/base/' changes from f1ed825..21c7655
    21c7655 feat: support 3.0.+
    ad03101 fix: merge some bug fix
    75f572d fix: use the better way to fix rendering problems
    9ab28a7 fix: `MetadataReceived` event parameters bug
    313cddd chore: add .gitignore
    c737102 fix: some rendering problems when change render widget order
    2520e7a style: format with 2 spaces
    66eeaad Merge commit '0eab47114cde933482f90879e177fd019430cafd' into dev/raw_data
    576473c Android add getNativeHandle
    0eab471 feat: Android add getNativeHandle
    66ed5b5 fix bug
    e0d1d08 * fix `setDefaultAudioRoutetoSpeakerphone` crash bug * add `setAudioSessionOperationRestriction` and `sendCustomReportMessage` method
    c82a9e1 finish
    9fa3009 iOS finished
    d69a73d android finished
    51b093b - fix crash when rendering view without `channelId` property - fix `RtcLocalView` freezes after rendering remote view
    10a3d01 - fix multiple channel render bug - remove `Types` from export, you can import enum or class by `import {} from 'react-native-agora'`
    d998401 - add `startPreview` `stopPreview`
    93621d3 - add `constructor` for typescript - fix Android `mapToChannelMediaInfo` crash - fix iOS `switchChannel` `sendMetadata` crash
    d1d40f5 Merge commit 'b67450678710cbc3ad4fcf72d8b53dd695d0a8e8' into beta/3.0.0
    b674506 Android finish
    bdd398f change Double to Number
    b17dd47 fix convert exception in mapToCameraCapturerConfiguration function
    cf24c0b fix bugs
    51f0ca3 fix bug from issue: https://github.com/syanbo/react-native-agora/issues/213 upgrade to 3.0.1
    
    git-subtree-dir: android/src/main/java/io/agora/rtc/base
    git-subtree-split: 21c7655c72f00e40d8f08c3be7abdd6f783f4630

--- RtcTextureView.kt
+++ RtcTextureView.kt
@@ -2,45 +2,65 @@ package io.agora.rtc.base
 
 import android.content.Context
 import android.widget.FrameLayout
+import io.agora.rtc.RtcChannel
 import io.agora.rtc.RtcEngine
 import io.agora.rtc.mediaio.AgoraTextureView
 import io.agora.rtc.mediaio.MediaIO
+import java.lang.ref.WeakReference
 
 class RtcTextureView(
-        context: Context
+  context: Context
 ) : FrameLayout(context) {
-    private var texture: AgoraTextureView = AgoraTextureView(context)
-    private var uid: Int = 0
+  private var texture: AgoraTextureView = AgoraTextureView(context)
+  private var uid: Int = 0
+  private var channel: WeakReference<RtcChannel>? = null
 
-    init {
-        texture.init(null)
-        texture.setBufferType(MediaIO.BufferType.BYTE_ARRAY)
-        texture.setPixelFormat(MediaIO.PixelFormat.I420)
-        addView(texture)
-    }
+  init {
+    texture.init(null)
+    texture.setBufferType(MediaIO.BufferType.BYTE_ARRAY)
+    texture.setPixelFormat(MediaIO.PixelFormat.I420)
+    addView(texture)
+  }
 
-    fun setMirror(engine: RtcEngine, mirror: Boolean) {
-        texture.setMirror(mirror)
-        setupVideo(engine)
-    }
+  fun setData(engine: RtcEngine, channel: RtcChannel?, uid: Int) {
+    this.channel = if (channel != null) WeakReference(channel) else null
+    this.uid = uid
+    setupVideoRenderer(engine)
+  }
 
-    fun setUid(engine: RtcEngine, uid: Int) {
-        this.uid = uid
-        setupVideo(engine)
-    }
+  fun setMirror(engine: RtcEngine, mirror: Boolean) {
+    texture.setMirror(mirror)
+    setupVideoRenderer(engine)
+  }
 
-    private fun setupVideo(engine: RtcEngine) {
-        if (uid == 0) {
-            engine.setLocalVideoRenderer(texture)
-        } else {
-            engine.setRemoteVideoRenderer(uid, texture)
-        }
+  fun resetVideoRender(engine: RtcEngine) {
+    if (uid == 0) {
+      engine.setLocalVideoRenderer(null)
+    } else {
+      channel?.get()?.let {
+        it.setRemoteVideoRenderer(uid, null)
+        return@resetVideoRender
+      }
+      engine.setRemoteVideoRenderer(uid, null)
     }
+  }
 
-    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        val width: Int = MeasureSpec.getSize(widthMeasureSpec)
-        val height: Int = MeasureSpec.getSize(heightMeasureSpec)
-        texture.layout(0, 0, width, height)
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+  private fun setupVideoRenderer(engine: RtcEngine) {
+    if (uid == 0) {
+      engine.setLocalVideoRenderer(texture)
+    } else {
+      channel?.get()?.let {
+        it.setRemoteVideoRenderer(uid, texture)
+        return@setupVideoRenderer
+      }
+      engine.setRemoteVideoRenderer(uid, texture)
     }
+  }
+
+  override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+    val width: Int = MeasureSpec.getSize(widthMeasureSpec)
+    val height: Int = MeasureSpec.getSize(heightMeasureSpec)
+    texture.layout(0, 0, width, height)
+    super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+  }
 }
