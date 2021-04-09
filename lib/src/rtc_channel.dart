e2338a40ec796130baffe3d6f887da4493722cc3
Jerry.Luo <396912848@qq.com>
2021-04-09 19:12:22 +0800

    fix(change method call): createDataStream

--- lib/src/rtc_channel.dart
+++ lib/src/rtc_channel.dart
@@ -317,7 +317,7 @@ class RtcChannel with RtcChannelInterface {
   @override
   Future<int> createDataStreamWithConfig(DataStreamConfig config) {
     return _invokeMethod(
-        'createDataStreamWithConfig', {'config': config.toJson()});
+        'createDataStream', {'config': config.toJson()});
   }
 
   @override
