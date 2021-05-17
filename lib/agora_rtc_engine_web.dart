366eef1924e89a6f3134648e70f1808f04f49344
HUI <luxuhui@agora.io>
2021-05-17 18:17:52 +0800

    chore: optimize import

--- lib/agora_rtc_engine_web.dart
+++ lib/agora_rtc_engine_web.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-
 // In order to *not* need this ignore, consider extracting the "web" version
 // of your plugin as a separate package, instead of inlining it in the same
 // package as the core of your plugin.
