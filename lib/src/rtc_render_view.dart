84cc405a808d2b6885725e677d24cfa6bc295096
HUI <luxuhui@agora.io>
2021-05-20 17:28:10 +0800

    fix: remove unless import

--- lib/src/rtc_render_view.dart
+++ lib/src/rtc_render_view.dart
@@ -1,11 +1,8 @@
-import 'dart:html';
-
 import 'package:flutter/foundation.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter/widgets.dart';
 
 import 'enum_converter.dart';
 import 'enums.dart';
