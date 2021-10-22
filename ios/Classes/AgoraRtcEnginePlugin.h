684f6c002753d23996518664577a0f7b515fd63e
littleGnAl <tearjeaker@hotmail.com>
2021-10-22 19:26:07 +0800

    Remove unnecessary comments

--- ios/Classes/AgoraRtcEnginePlugin.h
+++ ios/Classes/AgoraRtcEnginePlugin.h
@@ -3,17 +3,3 @@
 
 @interface AgoraRtcEnginePlugin : NSObject<FlutterPlugin>
 @end
-
-//@protocol RtcEnginePlugin
-//
-//- (void)onRtcEngineCreated:(AgoraRtcEngineKit *_Nullable)rtcEngine;
-//- (void)onRtcEngineDestroyed;
-//
-//@end
-
-//@interface RtcEnginePluginRegistrant : NSObject
-//
-//+(void)register:(NSObject<RtcEnginePlugin> *_Nonnull)plugin;
-//+(void)unregister:(NSObject<RtcEnginePlugin> *_Nonnull)plugin;
-//
-//@end
