a8cf0028de62df7eba32d2122bb81fdcc48013c7
littleGnAl <tearjeaker@hotmail.com>
2021-10-26 11:36:55 +0800

    feat: [Android] Implement custom audio source example

--- example/android/app/src/main/kotlin/io/agora/agora_rtc_engine_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/io/agora/agora_rtc_engine_example/MainActivity.kt
@@ -1,6 +1,31 @@
 package io.agora.agora_rtc_engine_example
 
+import android.os.Bundle
+import io.agora.agora_rtc_engine_example.custom_audio_source.CustomAudioPlugin
+import io.agora.agora_rtc_engine_example.custom_audio_source.CustomAudioSource
+import io.agora.rtc.base.RtcEnginePlugin
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import java.lang.ref.WeakReference
 
 class MainActivity: FlutterActivity() {
+
+  private val customAudioPlugin = CustomAudioPlugin(WeakReference(this))
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+
+    RtcEnginePlugin.register(customAudioPlugin)
+  }
+
+  override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+    super.configureFlutterEngine(flutterEngine)
+    CustomAudioSource.CustomAudioSourceApi.setup(flutterEngine.dartExecutor, customAudioPlugin)
+  }
+
+  override fun onDestroy() {
+    super.onDestroy()
+
+    RtcEnginePlugin.unregister(customAudioPlugin)
+  }
 }
