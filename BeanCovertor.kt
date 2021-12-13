c3a8b8dc2ee118bb19c05d4bd4b5db58c383eecb
HUI <luxuhui@agora.io>
2021-12-13 21:45:10 +0800

    Squashed 'android/src/main/java/io/agora/rtc/base/' changes from 21c7655..fbf0147
    fbf0147 fix: getAudioFileInfo & selectAudioTrack
    987de50 feat: support 3.5.2
    4ebf77c feat: support 3.5.1 & plugin
    27c788c chore: mapToColor
    0278629 feat: support 3.5.0.2
    45dcd16 chore: add API which is missing
    58ca4b0 fix: AudioRecordingConfiguration bug
    4a32e12 chore: prepare to migrate to iris
    276c9fe chore: support 3.4.5
    783aecf chore: add ERR_ALREADY_IN_RECORDING
    3aed6ce fix: startAudioRecording bug
    510b405 chore: optimize
    4f801b9 chore: optimize
    ab71f52 chore: optimize startAudioRecording
    07f81c7 chore: optimize
    5a20e13 style: optimize
    858b58f fix(uid compatible): int to uint
    8d14d2d feat(upgrade): 3.4.1
    be5efea Merge pull request #4 from AgoraLibrary/dev/3.3.0
    382f858 Merge branch 'master' into dev/3.3.0
    8671535 Merge pull request #3 from AgoraLibrary/dev/3.2.0
    feabda3 Merge branch 'master' into dev/3.2.0
    9e57891 Merge pull request #2 from AgoraLibrary/dev/3.1.0
    df303ee fix: merge some bug fix
    cd7a2cd feat: support 3.3.1 for Android
    551ebfe fix: annotation build warning
    9c48594 feat: support 3.3.0 for Android
    de52ca6 Merge branch 'master' into dev/3.2.0
    ddeaefb Merge branch 'master' into dev/3.2.0
    2fef3ae feat: add `setClientRole(role: ClientRole, options?: ClientRoleOptions): Promise<void>`
    fcb57cc feat: upgrade to 3.2.0
    REVERT: 21c7655 feat: support 3.0.+
    
    git-subtree-dir: android/src/main/java/io/agora/rtc/base
    git-subtree-split: fbf0147a1ba57f0eabe9c5b54fffeec83a7e2c14

--- BeanCovertor.kt
+++ BeanCovertor.kt
@@ -2,11 +2,17 @@ package io.agora.rtc.base
 
 import android.graphics.Color
 import io.agora.rtc.RtcEngineConfig
+import io.agora.rtc.audio.AgoraRhythmPlayerConfig
+import io.agora.rtc.audio.AudioRecordingConfiguration
+import io.agora.rtc.internal.EncryptionConfig
 import io.agora.rtc.internal.LastmileProbeConfig
 import io.agora.rtc.live.LiveInjectStreamConfig
 import io.agora.rtc.live.LiveTranscoding
 import io.agora.rtc.live.LiveTranscoding.TranscodingUser
 import io.agora.rtc.models.ChannelMediaOptions
+import io.agora.rtc.models.ClientRoleOptions
+import io.agora.rtc.models.DataStreamConfig
+import io.agora.rtc.models.EchoTestConfiguration
 import io.agora.rtc.video.*
 
 fun mapToVideoDimensions(map: Map<*, *>): VideoEncoderConfiguration.VideoDimensions {
@@ -24,7 +30,9 @@ fun mapToVideoEncoderConfiguration(map: Map<*, *>): VideoEncoderConfiguration {
     (map["bitrate"] as? Number)?.let { bitrate = it.toInt() }
     (map["minBitrate"] as? Number)?.let { minBitrate = it.toInt() }
     (map["orientationMode"] as? Number)?.let { orientationMode = intToOrientationMode(it.toInt()) }
-    (map["degradationPrefer"] as? Number)?.let { degradationPrefer = intToDegradationPreference(it.toInt()) }
+    (map["degradationPrefer"] as? Number)?.let {
+      degradationPrefer = intToDegradationPreference(it.toInt())
+    }
     (map["mirrorMode"] as? Number)?.let { mirrorMode = it.toInt() }
   }
 }
@@ -50,7 +58,7 @@ fun mapToAgoraImage(map: Map<*, *>): AgoraImage {
 
 fun mapToTranscodingUser(map: Map<*, *>): TranscodingUser {
   return TranscodingUser().apply {
-    (map["uid"] as? Number)?.let { uid = it.toInt() }
+    (map["uid"] as? Number)?.let { uid = it.toNativeUInt() }
     (map["x"] as? Number)?.let { x = it.toInt() }
     (map["y"] as? Number)?.let { y = it.toInt() }
     (map["width"] as? Number)?.let { width = it.toInt() }
@@ -79,11 +87,17 @@ fun mapToLiveTranscoding(map: Map<*, *>): LiveTranscoding {
     (map["videoGop"] as? Number)?.let { videoGop = it.toInt() }
     (map["watermark"] as? Map<*, *>)?.let { watermark = mapToAgoraImage(it) }
     (map["backgroundImage"] as? Map<*, *>)?.let { backgroundImage = mapToAgoraImage(it) }
-    (map["audioSampleRate"] as? Number)?.let { audioSampleRate = intToLiveTranscodingAudioSampleRate(it.toInt()) }
+    (map["audioSampleRate"] as? Number)?.let {
+      audioSampleRate = intToLiveTranscodingAudioSampleRate(it.toInt())
+    }
     (map["audioBitrate"] as? Number)?.let { audioBitrate = it.toInt() }
     (map["audioChannels"] as? Number)?.let { audioChannels = it.toInt() }
-    (map["audioCodecProfile"] as? Number)?.let { audioCodecProfile = intToAudioCodecProfile(it.toInt()) }
-    (map["videoCodecProfile"] as? Number)?.let { videoCodecProfile = intToVideoCodecProfile(it.toInt()) }
+    (map["audioCodecProfile"] as? Number)?.let {
+      audioCodecProfile = intToAudioCodecProfile(it.toInt())
+    }
+    (map["videoCodecProfile"] as? Number)?.let {
+      videoCodecProfile = intToVideoCodecProfile(it.toInt())
+    }
     (map["backgroundColor"] as? Map<*, *>)?.let { backgroundColor = mapToColor(it) }
     (map["userConfigExtraInfo"] as? String)?.let { userConfigExtraInfo = it }
     (map["transcodingUsers"] as? List<*>)?.let { list ->
@@ -100,7 +114,7 @@ fun mapToChannelMediaInfo(map: Map<*, *>): ChannelMediaInfo {
   return ChannelMediaInfo(
     map["channelName"] as? String,
     map["token"] as? String,
-    (map["uid"] as Number).toInt()
+    (map["uid"] as Number).toNativeUInt()
   )
 }
 
@@ -139,8 +153,12 @@ fun mapToRectangle(map: Map<*, *>): WatermarkOptions.Rectangle {
 fun mapToWatermarkOptions(map: Map<*, *>): WatermarkOptions {
   return WatermarkOptions().apply {
     (map["visibleInPreview"] as? Boolean)?.let { visibleInPreview = it }
-    (map["positionInLandscapeMode"] as? Map<*, *>)?.let { positionInLandscapeMode = mapToRectangle(it) }
-    (map["positionInPortraitMode"] as? Map<*, *>)?.let { positionInPortraitMode = mapToRectangle(it) }
+    (map["positionInLandscapeMode"] as? Map<*, *>)?.let {
+      positionInLandscapeMode = mapToRectangle(it)
+    }
+    (map["positionInPortraitMode"] as? Map<*, *>)?.let {
+      positionInPortraitMode = mapToRectangle(it)
+    }
   }
 }
 
@@ -151,9 +169,19 @@ fun mapToLiveInjectStreamConfig(map: Map<*, *>): LiveInjectStreamConfig {
     (map["videoGop"] as? Number)?.let { videoGop = it.toInt() }
     (map["videoFramerate"] as? Number)?.let { videoFramerate = it.toInt() }
     (map["videoBitrate"] as? Number)?.let { videoBitrate = it.toInt() }
-    (map["audioSampleRate"] as? Number)?.let { audioSampleRate = intToLiveInjectStreamConfigAudioSampleRate(it.toInt()) }
+    (map["audioSampleRate"] as? Number)?.let {
+      audioSampleRate = intToLiveInjectStreamConfigAudioSampleRate(it.toInt())
+    }
     (map["audioBitrate"] as? Number)?.let { audioBitrate = it.toInt() }
     (map["audioChannels"] as? Number)?.let { audioChannels = it.toInt() }
+  }
+}
+
+fun mapToRhythmPlayerConfig(map: Map<*, *>): AgoraRhythmPlayerConfig {
+  return AgoraRhythmPlayerConfig().apply {
+    (map["beatsPerMeasure"] as? Number)?.let { beatsPerMeasure = it.toInt() }
+    (map["beatsPerMinute"] as? Number)?.let { beatsPerMinute = it.toInt() }
+    (map["publish"] as? Boolean)?.let { publish = it }
   }
 }
 
@@ -161,13 +189,19 @@ fun mapToCameraCapturerConfiguration(map: Map<*, *>): CameraCapturerConfiguratio
   return CameraCapturerConfiguration(
     intToCapturerOutputPreference((map["preference"] as Number).toInt()),
     intToCameraDirection((map["cameraDirection"] as Number).toInt())
-  )
+  ).apply {
+    dimensions = CameraCapturerConfiguration.CaptureDimensions()
+    (map["captureWidth"] as? Number)?.toInt()?.let { dimensions.width = it }
+    (map["captureHeight"] as? Number)?.toInt()?.let { dimensions.height = it }
+  }
 }
 
 fun mapToChannelMediaOptions(map: Map<*, *>): ChannelMediaOptions {
   return ChannelMediaOptions().apply {
     (map["autoSubscribeAudio"] as? Boolean)?.let { autoSubscribeAudio = it }
     (map["autoSubscribeVideo"] as? Boolean)?.let { autoSubscribeVideo = it }
+    (map["publishLocalAudio"] as? Boolean)?.let { publishLocalAudio = it }
+    (map["publishLocalVideo"] as? Boolean)?.let { publishLocalVideo = it }
   }
 }
 
@@ -175,5 +209,68 @@ fun mapToRtcEngineConfig(map: Map<*, *>): RtcEngineConfig {
   return RtcEngineConfig().apply {
     mAppId = map["appId"] as String
     (map["areaCode"] as? Number)?.toInt()?.let { mAreaCode = it }
+    (map["logConfig"] as? Map<*, *>)?.let { mLogConfig = mapToLogConfig(it) }
+  }
+}
+
+fun mapToAudioRecordingConfiguration(map: Map<*, *>): AudioRecordingConfiguration {
+  return AudioRecordingConfiguration().apply {
+    (map["filePath"] as? String)?.let { filePath = it }
+    (map["recordingQuality"] as? Number)?.let { recordingQuality = it.toInt() }
+    (map["recordingPosition"] as? Number)?.let { recordingPosition = it.toInt() }
+    (map["recordingSampleRate"] as? Number)?.let { recordingSampleRate = it.toInt() }
+  }
+}
+
+fun mapToEncryptionConfig(map: Map<*, *>): EncryptionConfig {
+  return EncryptionConfig().apply {
+    (map["encryptionMode"] as? Number)?.let { encryptionMode = intToEncryptionMode(it.toInt()) }
+    (map["encryptionKey"] as? String)?.let { encryptionKey = it }
+    (map["encryptionKdfSalt"] as? List<*>)?.let { list ->
+      for (i in list.indices) {
+        (list[i] as? Number)?.let {
+          encryptionKdfSalt[i] = it.toByte()
+        }
+      }
+    }
+  }
+}
+
+fun mapToClientRoleOptions(map: Map<*, *>): ClientRoleOptions {
+  return ClientRoleOptions().apply {
+    (map["audienceLatencyLevel"] as? Number)?.let { audienceLatencyLevel = it.toInt() }
+  }
+}
+
+fun mapToLogConfig(map: Map<*, *>): RtcEngineConfig.LogConfig {
+  return RtcEngineConfig.LogConfig().apply {
+    (map["filePath"] as? String)?.let { filePath = it }
+    (map["fileSize"] as? Number)?.let { fileSize = it.toInt() }
+    (map["level"] as? Number)?.let { level = it.toInt() }
+  }
+}
+
+fun mapToDataStreamConfig(map: Map<*, *>): DataStreamConfig {
+  return DataStreamConfig().apply {
+    (map["syncWithAudio"] as? Boolean)?.let { syncWithAudio = it }
+    (map["ordered"] as? Boolean)?.let { ordered = it }
+  }
+}
+
+fun mapToVirtualBackgroundSource(map: Map<*, *>): VirtualBackgroundSource {
+  return VirtualBackgroundSource().apply {
+    (map["backgroundSourceType"] as? Number)?.let { backgroundSourceType = it.toInt() }
+    (map["color"] as? Map<*, *>)?.let { color = mapToColor(it) }
+    (map["source"] as? String)?.let { source = it }
+    (map["blur_degree"] as? Int)?.let { blur_degree = it }
+  }
+}
+
+fun mapToEchoTestConfiguration(map: Map<*, *>): EchoTestConfiguration {
+  return EchoTestConfiguration().apply {
+    (map["enableAudio"] as? Boolean)?.let { enableAudio = it }
+    (map["enableVideo"] as? Boolean)?.let { enableVideo = it }
+    (map["token"] as? String)?.let { token = it }
+    (map["channelId"] as? String)?.let { channelId = it }
   }
 }
