27812c32669f62f3b7e344db2128f6fb295fd630
HUI <luxuhui@agora.io>
2021-12-14 00:13:34 +0800

    test: fix iOS UT issue

--- example/ios/RunnerTests/OCTestRtcEnginePlugin.h
+++ example/ios/RunnerTests/OCTestRtcEnginePlugin.h
@@ -1,5 +1,8 @@
 #import <Flutter/Flutter.h>
-#import <RtcEnginePlugin.h>
+#if __has_include(<agora_rtc_engine/agora_rtc_engine-Swift.h>)
+#import <agora_rtc_engine/agora_rtc_engine-Swift.h>
+#endif
+#import <agora_rtc_engine/RtcEnginePlugin.h>
 
 @interface OCTestRtcEnginePlugin : NSObject<RtcEnginePlugin>
 @property bool isRtcEngineCreated;
--- example/ios/RunnerTests/RunnerTests-Bridging-Header.h
+++ example/ios/RunnerTests/RunnerTests-Bridging-Header.h
@@ -3,4 +3,8 @@
 
 #import "OCTestRtcEnginePlugin.h"
 
+#if __has_include(<agora_rtc_engine/agora_rtc_engine-Swift.h>)
+#import <agora_rtc_engine/agora_rtc_engine-Swift.h>
+#endif
+
 #endif /* RunnerTests_Bridging_Header_h */
