19697f366e3364ac62079523e53170357d15d6b1
littleGnAl <tearjeaker@hotmail.com>
2022-03-12 01:16:07 +0800

    fix: Fix pana static analyze

--- lib/src/enum_converter.g.dart
+++ lib/src/enum_converter.g.dart
@@ -1,5 +1,7 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
+// ignore_for_file: non_constant_identifier_names, deprecated_member_use_from_same_package, unused_element
+
 part of 'enum_converter.dart';
 
 // **************************************************************************
