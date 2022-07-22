e9720267f124c28fa0e06e101893eb591f56b345
Littlegnal <8847263+littleGnAl@users.noreply.github.com>
2022-07-22 21:20:15 +0800

    feat: Convert kotlin to java to avoid kotlin version conflict (#2)
    * feat: Convert kotlin to java to avoid kotlin version conflict

---
+++ android/src/main/java/io/agora/agora_rtc_ng/AgoraRtcNgPlugin.java
@@ -0,0 +1,84 @@
+package io.agora.agora_rtc_ng;
+
+import androidx.annotation.NonNull;
+
+import java.io.IOException;
+import java.lang.ref.WeakReference;
+
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
+
+public class AgoraRtcNgPlugin implements FlutterPlugin, MethodChannel.MethodCallHandler {
+
+    static {
+        System.loadLibrary("AgoraRtcWrapper");
+    }
+
+    private MethodChannel channel;
+    private WeakReference<FlutterPluginBinding> flutterPluginBindingRef;
+    private VideoViewController videoViewController;
+
+    @Override
+    public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
+        channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "agora_rtc_ng");
+        channel.setMethodCallHandler(this);
+        flutterPluginBindingRef = new WeakReference<>(flutterPluginBinding);
+
+        flutterPluginBinding.getPlatformViewRegistry().registerViewFactory(
+                "AgoraTextureView",
+                new AgoraPlatformViewFactory(
+                        "AgoraTextureView",
+                        flutterPluginBinding.getBinaryMessenger(),
+                        new AgoraPlatformViewFactory.PlatformViewProviderTextureView()));
+
+        flutterPluginBinding.getPlatformViewRegistry().registerViewFactory(
+                "AgoraSurfaceView",
+                new AgoraPlatformViewFactory(
+                        "AgoraSurfaceView",
+                        flutterPluginBinding.getBinaryMessenger(),
+                        new AgoraPlatformViewFactory.PlatformViewProviderSurfaceView()));
+
+        videoViewController = new VideoViewController(flutterPluginBinding.getBinaryMessenger());
+    }
+
+    @Override
+    public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+        channel.setMethodCallHandler(null);
+        videoViewController.dispose();
+    }
+
+    @Override
+    public void onMethodCall(@NonNull MethodCall call, @NonNull MethodChannel.Result result) {
+        if ("getAssetAbsolutePath".equals(call.method)) {
+            getAssetAbsolutePath(call, result);
+        } else {
+            result.notImplemented();
+        }
+    }
+
+    private void getAssetAbsolutePath(MethodCall call, MethodChannel.Result result) {
+        final String path = call.arguments();
+
+        if (path != null) {
+            if (this.flutterPluginBindingRef.get() != null
+            ) {
+                final String assetKey = this.flutterPluginBindingRef.get()
+                        .getFlutterAssets()
+                        .getAssetFilePathByName(path);
+                try {
+                    this.flutterPluginBindingRef.get().getApplicationContext()
+                            .getAssets()
+                            .openFd(assetKey)
+                            .close();
+                    result.success("/assets/" + assetKey);
+                    return;
+                } catch (IOException e) {
+                    result.error(e.getClass().getSimpleName(), e.getMessage(), e.getCause());
+                    return;
+                }
+            }
+        }
+        result.error("IllegalArgumentException", "The parameter should not be null", null);
+    }
+}
---
+++ android/src/main/java/io/agora/agora_rtc_ng/VideoViewController.java
@@ -0,0 +1,54 @@
+package io.agora.agora_rtc_ng;
+
+import androidx.annotation.NonNull;
+
+import io.flutter.plugin.common.BinaryMessenger;
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
+
+public class VideoViewController implements MethodChannel.MethodCallHandler {
+
+    private final MethodChannel methodChannel;
+
+    VideoViewController(BinaryMessenger binaryMessenger) {
+        methodChannel = new MethodChannel(binaryMessenger, "agora_rtc_ng/video_view_controller");
+        methodChannel.setMethodCallHandler(this);
+    }
+
+    private long createPlatformRender(){
+        return 0L;
+    }
+
+    private boolean destroyPlatformRender(long platformRenderId) {
+        return true;
+    }
+
+    private long createTextureRender() {
+        return 0L;
+    }
+
+    private boolean destroyTextureRender(long textureId){
+        return false;
+    }
+
+    @Override
+    public void onMethodCall(@NonNull MethodCall call, @NonNull MethodChannel.Result result) {
+        switch (call.method) {
+            case "attachVideoFrameBufferManager":
+            case "detachVideoFrameBufferManager":
+                result.success(true);
+                break;
+
+            case "createTextureRender":
+            case "destroyTextureRender":
+            case "updateTextureRenderData":
+            default:
+                result.notImplemented();
+                break;
+        }
+    }
+
+    public void dispose() {
+        methodChannel.setMethodCallHandler(null);
+    }
+}
