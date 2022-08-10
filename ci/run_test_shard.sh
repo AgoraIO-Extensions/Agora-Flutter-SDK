e2e89715d358fd994f14b9472719cc7b43a180a3
littleGnAl <tearjeaker@hotmail.com>
2022-08-10 20:33:42 +0800

    WIP

--- ci/run_test_shard.sh
+++ ci/run_test_shard.sh
@@ -8,7 +8,7 @@ flutter packages get
 flutter build ios --no-codesign --simulator --debug
 popd
 
-pushd test_shard/destroy_irisengine_smoke_test
+pushd test_shard/destroy_irisengine_smoke_test/ios
 
 bundle install
 bundle exec fastlane tests --verbose
