AgoraIO-Extensions/agora_rtc_ng_flutter

feat: Upgrade native sdk 4.0.0-rc.1

example/lib/examples/advanced/index.dart
@@ -1,6 +1,11 @@
 import 'dart:io';
 
 import 'package:agora_rtc_ng_example/examples/advanced/media_player/media_player.dart';
+import 'package:agora_rtc_ng_example/examples/advanced/process_audio_raw_data/process_audio_raw_data.dart';
+import 'package:agora_rtc_ng_example/examples/advanced/process_video_raw_data/process_video_raw_data.dart';
+import 'package:agora_rtc_ng_example/examples/advanced/push_audio_frame/push_audio_frame.dart';
+import 'package:agora_rtc_ng_example/examples/advanced/push_encoded_video_frame/push_encoded_video_frame.dart';
+import 'package:agora_rtc_ng_example/examples/advanced/push_video_frame/push_video_frame.dart';
 import 'package:agora_rtc_ng_example/examples/advanced/rtmp_streaming/rtmp_streaming.dart';
 import 'package:agora_rtc_ng_example/examples/advanced/screen_sharing/screen_sharing.dart';
 import 'package:agora_rtc_ng_example/examples/advanced/send_multi_camera_stream/send_multi_camera_stream.dart';
@@ -8,6 +13,7 @@ import 'package:agora_rtc_ng_example/examples/advanced/send_multi_video_stream/s
 import 'package:agora_rtc_ng_example/examples/advanced/set_beauty_effect/set_beauty_effect.dart';
 import 'package:agora_rtc_ng_example/examples/advanced/set_encryption/set_encryption.dart';
 import 'package:agora_rtc_ng_example/examples/advanced/set_video_encoder_configuration/set_video_encoder_configuration.dart';
+import 'package:agora_rtc_ng_example/examples/advanced/spatial_audio_with_media_player/spatial_audio_with_media_player.dart';
 import 'package:agora_rtc_ng_example/examples/advanced/start_direct_cdn_streaming/start_direct_cdn_streaming.dart';
 import 'package:agora_rtc_ng_example/examples/advanced/start_local_video_transcoder/start_local_video_transcoder.dart';
 import 'package:agora_rtc_ng_example/examples/advanced/stream_message/stream_message.dart';
@@ -15,11 +21,14 @@ import 'package:agora_rtc_ng_example/examples/advanced/take_snapshot/take_snapsh
 import 'package:flutter/foundation.dart';
 
 import 'audio_mixing/audio_mixing.dart';
+import 'audio_spectrum/audio_spectrum.dart';
 import 'channel_media_relay/channel_media_relay.dart';
 import 'device_manager/device_manager.dart';
 import 'enable_virtualbackground/enable_virtualbackground.dart';
 import 'join_multiple_channel/join_multiple_channel.dart';
+import 'media_recorder/media_recorder.dart';
 import 'send_metadata/send_metadata.dart';
+import 'set_content_inspect/set_content_inspect.dart';
 import 'start_rhythm_player/start_rhythm_player.dart';
 import 'voice_changer/voice_changer.dart';
 
@@ -34,7 +43,7 @@ final advanced = [
     {'name': 'DeviceManager', 'widget': const DeviceManager()},
   {'name': 'JoinMultipleChannel', 'widget': const JoinMultipleChannel()},
   {'name': 'RtmpStreaming', 'widget': const RtmpStreaming()},
-  if (kIsWeb || !(Platform.isAndroid || Platform.isIOS))
+  if (kIsWeb || !Platform.isIOS)
     {'name': 'ScreenSharing', 'widget': const ScreenSharing()},
   {'name': 'SetEncryption', 'widget': SetEncryption()},
   {
@@ -56,8 +65,7 @@ final advanced = [
   },
   {'name': 'SendMetadata', 'widget': const SendMetadata()},
   {'name': 'SetBeautyEffect', 'widget': const SetBeautyEffect()},
-  // TODO(littlegnal): Not support yet (MS-187328).
-  // {'name': 'SetContentInspect', 'widget': const SetContentInspect()},
+  {'name': 'SetContentInspect', 'widget': const SetContentInspect()},
   if (kIsWeb || !(Platform.isAndroid || Platform.isIOS))
     {'name': 'SendMultiCameraStream', 'widget': const SendMultiCameraStream()},
   {'name': 'StartRhythmPlayer', 'widget': const StartRhythmPlayer()},
@@ -66,4 +74,15 @@ final advanced = [
       'name': 'StartLocalVideoTranscoder',
       'widget': const StartLocalVideoTranscoder()
     },
+  // {'name': 'ProcessVideoRawData', 'widget': const ProcessVideoRawData()},
+  // {'name': 'ProcessAudioRawData', 'widget': const ProcessAudioRawData()},
+  {'name': 'AudioSpectrum', 'widget': const AudioSpectrum()},
+  {'name': 'MediaRecorder', 'widget': const MediaRecorder()},
+  {'name': 'PushVideoFrame', 'widget': const PushVideoFrame()},
+  // {'name': 'PushAudioFrame', 'widget': const PushAudioFrame()},
+  {'name': 'PushEncodedVideoFrame', 'widget': const PushEncodedVideoFrame()},
+  {
+    'name': 'SpatialAudioWithMediaPlayer',
+    'widget': const SpatialAudioWithMediaPlayer()
+  },
 ];

example/lib/examples/advanced/push_encoded_video_frame/push_encoded_video_frame.dart
@@ -1,0 +1,222 @@
+import 'dart:convert';
+import 'dart:typed_data';
+
+import 'package:agora_rtc_ng/agora_rtc_ng.dart';
+import 'package:agora_rtc_ng_example/config/agora.config.dart' as config;
+import 'package:agora_rtc_ng_example/examples/example_actions_widget.dart';
+import 'package:agora_rtc_ng_example/examples/log_sink.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+/// PushEncodedVideoFrame Example
+class PushEncodedVideoFrame extends StatefulWidget {
+  /// Construct the [PushEncodedVideoFrame]
+  const PushEncodedVideoFrame({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _State();
+}
+
+class _State extends State<PushEncodedVideoFrame> {
+  late final RtcEngine _engine;
+  bool _isReadyPreview = false;
+  bool isJoined = false;
+  Set<int> remoteUids = {};
+  late final TextEditingController _channelIdController;
+  final TextEditingController _controller = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _channelIdController = TextEditingController(text: config.channelId);
+    _initEngine();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+
+    _dispose();
+  }
+
+  Future<void> _dispose() async {
+    await _engine.release();
+  }
+
+  Future<void> _initEngine() async {
+    _engine = createAgoraRtcEngine();
+    await _engine.initialize(RtcEngineContext(
+      appId: config.appId,
+      channelProfile: ChannelProfileType.channelProfileLiveBroadcasting,
+    ));
+
+    _engine.registerEventHandler(RtcEngineEventHandler(
+      onError: (ErrorCodeType err, String msg) {
+        logSink.log('[onError] err: $err, msg: $msg');
+      },
+      onJoinChannelSuccess: (RtcConnection connection, int elapsed) {
+        logSink.log(
+            '[onJoinChannelSuccess] connection: ${connection.toJson()} elapsed: $elapsed');
+        setState(() {
+          isJoined = true;
+        });
+      },
+      onLeaveChannel: (RtcConnection connection, RtcStats stats) {
+        logSink.log(
+            '[onLeaveChannel] connection: ${connection.toJson()} stats: ${stats.toJson()}');
+        setState(() {
+          isJoined = false;
+        });
+      },
+      onUserJoined:
+          (RtcConnection connection, int remoteUid, int elapsed) async {
+        await _engine.setRemoteVideoSubscriptionOptions(
+            uid: remoteUid,
+            options: const VideoSubscriptionOptions(encodedFrameOnly: true));
+      },
+    ));
+
+    await _engine.getMediaEngine().setExternalVideoSource(
+        enabled: true,
+        useTexture: false,
+        sourceType: ExternalVideoSourceType.encodedVideoFrame,
+        encodedVideoOption:
+            const SenderOptions(codecType: VideoCodecType.videoCodecGeneric));
+
+    // enable video module and set up video encoding configs
+    await _engine.enableVideo();
+
+    _engine
+        .getMediaEngine()
+        .registerVideoEncodedFrameObserver(VideoEncodedFrameObserver(
+      onEncodedVideoFrameReceived:
+          (uid, imageBuffer, length, videoEncodedFrameInfo) {
+        debugPrint(
+            '[onEncodedVideoFrameReceived] uid: $uid imageBuffer: $imageBuffer, length: $length, videoEncodedFrameInfo: ${videoEncodedFrameInfo.toJson()}');
+        try {
+          _showMyDialog(uid, utf8.decode(imageBuffer));
+        } catch (e) {
+          debugPrint(e.toString());
+        }
+      },
+    ));
+
+    // make this room live broadcasting room
+    await _engine.setClientRole(role: ClientRoleType.clientRoleBroadcaster);
+
+    setState(() {
+      _isReadyPreview = true;
+    });
+  }
+
+  Future<void> _joinChannel() async {
+    if (defaultTargetPlatform == TargetPlatform.android) {
+      await [Permission.microphone, Permission.camera].request();
+    }
+    await _engine.joinChannel(
+        token: config.token,
+        channelId: _channelIdController.text,
+        info: '',
+        uid: config.uid);
+  }
+
+  Future<void> _leaveChannel() async {
+    await _engine.leaveChannel();
+  }
+
+  Future<void> _showMyDialog(int uid, String data) async {
+    return showDialog(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Receive from uid:$uid'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[Text(data)],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Ok'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Future<void> _onPressSend() async {
+    if (_controller.text.isEmpty) {
+      return;
+    }
+
+    final data = Uint8List.fromList(utf8.encode(_controller.text));
+    await _engine.getMediaEngine().pushEncodedVideoImage(
+        imageBuffer: data,
+        length: data.length,
+        videoEncodedFrameInfo: const EncodedVideoFrameInfo(
+            framesPerSecond: 60,
+            codecType: VideoCodecType.videoCodecGeneric,
+            frameType: VideoFrameType.videoFrameTypeKeyFrame));
+
+    _controller.clear();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ExampleActionsWidget(
+      displayContentBuilder: (context, isLayoutHorizontal) {
+        if (!_isReadyPreview) return Container();
+
+        return const Center(
+          child: Text('No Preview'),
+        );
+      },
+      actionsBuilder: (context, isLayoutHorizontal) {
+        return Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextField(
+              controller: _channelIdController,
+              decoration: const InputDecoration(hintText: 'Channel ID'),
+            ),
+            Row(
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: ElevatedButton(
+                    onPressed: isJoined ? _leaveChannel : _joinChannel,
+                    child: Text('${isJoined ? 'Leave' : 'Join'} channel'),
+                  ),
+                )
+              ],
+            ),
+            if (isJoined)
+              Row(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Expanded(
+                      child: TextField(
+                          controller: _controller,
+                          decoration: const InputDecoration(
+                            hintText: 'Input Message',
+                          ))),
+                  ElevatedButton(
+                    onPressed: _onPressSend,
+                    child: const Text('Send'),
+                  ),
+                ],
+              )
+          ],
+        );
+      },
+    );
+  }
+}

AgoraIO-Extensions/Agora-Flutter-SDK

example/lib/examples/advanced/index.dart
@@ -1,25 +1,32 @@
 import 'dart:io';
 
-import 'package:agora_rtc_ng_example/examples/advanced/media_player/media_player.dart';
-import 'package:agora_rtc_ng_example/examples/advanced/rtmp_streaming/rtmp_streaming.dart';
-import 'package:agora_rtc_ng_example/examples/advanced/screen_sharing/screen_sharing.dart';
-import 'package:agora_rtc_ng_example/examples/advanced/send_multi_camera_stream/send_multi_camera_stream.dart';
-import 'package:agora_rtc_ng_example/examples/advanced/send_multi_video_stream/send_multi_video_stream.dart';
-import 'package:agora_rtc_ng_example/examples/advanced/set_beauty_effect/set_beauty_effect.dart';
-import 'package:agora_rtc_ng_example/examples/advanced/set_encryption/set_encryption.dart';
-import 'package:agora_rtc_ng_example/examples/advanced/set_video_encoder_configuration/set_video_encoder_configuration.dart';
-import 'package:agora_rtc_ng_example/examples/advanced/start_direct_cdn_streaming/start_direct_cdn_streaming.dart';
-import 'package:agora_rtc_ng_example/examples/advanced/start_local_video_transcoder/start_local_video_transcoder.dart';
-import 'package:agora_rtc_ng_example/examples/advanced/stream_message/stream_message.dart';
-import 'package:agora_rtc_ng_example/examples/advanced/take_snapshot/take_snapshot.dart';
+import 'package:agora_rtc_engine_example/examples/advanced/media_player/media_player.dart';
+import 'package:agora_rtc_engine_example/examples/advanced/push_encoded_video_frame/push_encoded_video_frame.dart';
+import 'package:agora_rtc_engine_example/examples/advanced/push_video_frame/push_video_frame.dart';
+import 'package:agora_rtc_engine_example/examples/advanced/rtmp_streaming/rtmp_streaming.dart';
+import 'package:agora_rtc_engine_example/examples/advanced/screen_sharing/screen_sharing.dart';
+import 'package:agora_rtc_engine_example/examples/advanced/send_multi_camera_stream/send_multi_camera_stream.dart';
+import 'package:agora_rtc_engine_example/examples/advanced/send_multi_video_stream/send_multi_video_stream.dart';
+import 'package:agora_rtc_engine_example/examples/advanced/set_beauty_effect/set_beauty_effect.dart';
+import 'package:agora_rtc_engine_example/examples/advanced/set_encryption/set_encryption.dart';
+import 'package:agora_rtc_engine_example/examples/advanced/set_video_encoder_configuration/set_video_encoder_configuration.dart';
+import 'package:agora_rtc_engine_example/examples/advanced/spatial_audio_with_media_player/spatial_audio_with_media_player.dart';
+import 'package:agora_rtc_engine_example/examples/advanced/start_direct_cdn_streaming/start_direct_cdn_streaming.dart';
+import 'package:agora_rtc_engine_example/examples/advanced/start_local_video_transcoder/start_local_video_transcoder.dart';
+import 'package:agora_rtc_engine_example/examples/advanced/stream_message/stream_message.dart';
+import 'package:agora_rtc_engine_example/examples/advanced/take_snapshot/take_snapshot.dart';
 import 'package:flutter/foundation.dart';
 
 import 'audio_mixing/audio_mixing.dart';
+import 'audio_spectrum/audio_spectrum.dart';
 import 'channel_media_relay/channel_media_relay.dart';
 import 'device_manager/device_manager.dart';
 import 'enable_virtualbackground/enable_virtualbackground.dart';
 import 'join_multiple_channel/join_multiple_channel.dart';
+import 'media_recorder/media_recorder.dart';
+import 'precall_test/precall_test.dart';
 import 'send_metadata/send_metadata.dart';
+import 'set_content_inspect/set_content_inspect.dart';
 import 'start_rhythm_player/start_rhythm_player.dart';
 import 'voice_changer/voice_changer.dart';
 
@@ -34,8 +41,7 @@ final advanced = [
     {'name': 'DeviceManager', 'widget': const DeviceManager()},
   {'name': 'JoinMultipleChannel', 'widget': const JoinMultipleChannel()},
   {'name': 'RtmpStreaming', 'widget': const RtmpStreaming()},
-  if (kIsWeb || !(Platform.isAndroid || Platform.isIOS))
-    {'name': 'ScreenSharing', 'widget': const ScreenSharing()},
+  if (!kIsWeb) {'name': 'ScreenSharing', 'widget': const ScreenSharing()},
   {'name': 'SetEncryption', 'widget': SetEncryption()},
   {
     'name': 'SetVideoEncoderConfiguration',
@@ -56,8 +62,7 @@ final advanced = [
   },
   {'name': 'SendMetadata', 'widget': const SendMetadata()},
   {'name': 'SetBeautyEffect', 'widget': const SetBeautyEffect()},
-  // TODO(littlegnal): Not support yet (MS-187328).
-  // {'name': 'SetContentInspect', 'widget': const SetContentInspect()},
+  {'name': 'SetContentInspect', 'widget': const SetContentInspect()},
   if (kIsWeb || !(Platform.isAndroid || Platform.isIOS))
     {'name': 'SendMultiCameraStream', 'widget': const SendMultiCameraStream()},
   {'name': 'StartRhythmPlayer', 'widget': const StartRhythmPlayer()},
@@ -66,4 +71,17 @@ final advanced = [
       'name': 'StartLocalVideoTranscoder',
       'widget': const StartLocalVideoTranscoder()
     },
+  // {'name': 'ProcessVideoRawData', 'widget': const ProcessVideoRawData()},
+  // {'name': 'ProcessAudioRawData', 'widget': const ProcessAudioRawData()},
+  {'name': 'AudioSpectrum', 'widget': const AudioSpectrum()},
+  {'name': 'MediaRecorder', 'widget': const MediaRecorder()},
+  {'name': 'PushVideoFrame', 'widget': const PushVideoFrame()},
+  // {'name': 'PushAudioFrame', 'widget': const PushAudioFrame()},
+  {'name': 'PushEncodedVideoFrame', 'widget': const PushEncodedVideoFrame()},
+  {
+    'name': 'SpatialAudioWithMediaPlayer',
+    'widget': const SpatialAudioWithMediaPlayer()
+  },
+  if (kIsWeb || !(Platform.isAndroid || Platform.isIOS))
+    {'name': 'PreCallTest', 'widget': const PreCallTest()},
 ];

example/lib/examples/advanced/push_encoded_video_frame/push_encoded_video_frame.dart
@@ -1,0 +1,222 @@
+import 'dart:convert';
+import 'dart:typed_data';
+
+import 'package:agora_rtc_engine/agora_rtc_engine.dart';
+import 'package:agora_rtc_engine_example/config/agora.config.dart' as config;
+import 'package:agora_rtc_engine_example/components/example_actions_widget.dart';
+import 'package:agora_rtc_engine_example/components/log_sink.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+/// PushEncodedVideoFrame Example
+class PushEncodedVideoFrame extends StatefulWidget {
+  /// Construct the [PushEncodedVideoFrame]
+  const PushEncodedVideoFrame({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _State();
+}
+
+class _State extends State<PushEncodedVideoFrame> {
+  late final RtcEngine _engine;
+  bool _isReadyPreview = false;
+  bool isJoined = false;
+  Set<int> remoteUids = {};
+  late final TextEditingController _channelIdController;
+  final TextEditingController _controller = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _channelIdController = TextEditingController(text: config.channelId);
+    _initEngine();
+  }
+
+  @override
+  void dispose() {
+    _dispose();
+    super.dispose();
+  }
+
+  Future<void> _dispose() async {
+    await _engine.leaveChannel();
+    await _engine.release();
+  }
+
+  Future<void> _initEngine() async {
+    _engine = createAgoraRtcEngine();
+    await _engine.initialize(RtcEngineContext(
+      appId: config.appId,
+      channelProfile: ChannelProfileType.channelProfileLiveBroadcasting,
+    ));
+
+    _engine.registerEventHandler(RtcEngineEventHandler(
+      onError: (ErrorCodeType err, String msg) {
+        logSink.log('[onError] err: $err, msg: $msg');
+      },
+      onJoinChannelSuccess: (RtcConnection connection, int elapsed) {
+        logSink.log(
+            '[onJoinChannelSuccess] connection: ${connection.toJson()} elapsed: $elapsed');
+        setState(() {
+          isJoined = true;
+        });
+      },
+      onLeaveChannel: (RtcConnection connection, RtcStats stats) {
+        logSink.log(
+            '[onLeaveChannel] connection: ${connection.toJson()} stats: ${stats.toJson()}');
+        setState(() {
+          isJoined = false;
+        });
+      },
+      onUserJoined:
+          (RtcConnection connection, int remoteUid, int elapsed) async {
+        await _engine.setRemoteVideoSubscriptionOptions(
+            uid: remoteUid,
+            options: const VideoSubscriptionOptions(encodedFrameOnly: true));
+      },
+    ));
+
+    await _engine.getMediaEngine().setExternalVideoSource(
+        enabled: true,
+        useTexture: false,
+        sourceType: ExternalVideoSourceType.encodedVideoFrame,
+        encodedVideoOption:
+            const SenderOptions(codecType: VideoCodecType.videoCodecGeneric));
+
+    // enable video module and set up video encoding configs
+    await _engine.enableVideo();
+
+    _engine
+        .getMediaEngine()
+        .registerVideoEncodedFrameObserver(VideoEncodedFrameObserver(
+      onEncodedVideoFrameReceived:
+          (uid, imageBuffer, length, videoEncodedFrameInfo) {
+        debugPrint(
+            '[onEncodedVideoFrameReceived] uid: $uid imageBuffer: $imageBuffer, length: $length, videoEncodedFrameInfo: ${videoEncodedFrameInfo.toJson()}');
+        try {
+          _showMyDialog(uid, utf8.decode(imageBuffer));
+        } catch (e) {
+          debugPrint(e.toString());
+        }
+      },
+    ));
+
+    // make this room live broadcasting room
+    await _engine.setClientRole(role: ClientRoleType.clientRoleBroadcaster);
+
+    setState(() {
+      _isReadyPreview = true;
+    });
+  }
+
+  Future<void> _joinChannel() async {
+    if (defaultTargetPlatform == TargetPlatform.android) {
+      await [Permission.microphone, Permission.camera].request();
+    }
+    await _engine.joinChannel(
+        token: config.token,
+        channelId: _channelIdController.text,
+        uid: config.uid,
+        options: const ChannelMediaOptions());
+  }
+
+  Future<void> _leaveChannel() async {
+    await _engine.leaveChannel();
+  }
+
+  Future<void> _showMyDialog(int uid, String data) async {
+    return showDialog(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Receive from uid:$uid'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[Text(data)],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Ok'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Future<void> _onPressSend() async {
+    if (_controller.text.isEmpty) {
+      return;
+    }
+
+    final data = Uint8List.fromList(utf8.encode(_controller.text));
+    await _engine.getMediaEngine().pushEncodedVideoImage(
+        imageBuffer: data,
+        length: data.length,
+        videoEncodedFrameInfo: const EncodedVideoFrameInfo(
+            framesPerSecond: 60,
+            codecType: VideoCodecType.videoCodecGeneric,
+            frameType: VideoFrameType.videoFrameTypeKeyFrame));
+
+    _controller.clear();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ExampleActionsWidget(
+      displayContentBuilder: (context, isLayoutHorizontal) {
+        if (!_isReadyPreview) return Container();
+
+        return const Center(
+          child: Text('No Preview'),
+        );
+      },
+      actionsBuilder: (context, isLayoutHorizontal) {
+        return Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextField(
+              controller: _channelIdController,
+              decoration: const InputDecoration(hintText: 'Channel ID'),
+            ),
+            Row(
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: ElevatedButton(
+                    onPressed: isJoined ? _leaveChannel : _joinChannel,
+                    child: Text('${isJoined ? 'Leave' : 'Join'} channel'),
+                  ),
+                )
+              ],
+            ),
+            if (isJoined)
+              Row(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Expanded(
+                      child: TextField(
+                          controller: _controller,
+                          decoration: const InputDecoration(
+                            hintText: 'Input Message',
+                          ))),
+                  ElevatedButton(
+                    onPressed: _onPressSend,
+                    child: const Text('Send'),
+                  ),
+                ],
+              )
+          ],
+        );
+      },
+    );
+  }
+}