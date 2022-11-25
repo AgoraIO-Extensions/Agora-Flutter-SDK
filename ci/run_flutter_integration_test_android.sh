ccafb16baa8dc54baaf258348c2e369feec60438
littleGnAl <tearjeaker@hotmail.com>
2022-11-25 17:07:18 +0800

    Upgrade native sdk 4.1.0

--- ci/run_flutter_integration_test_android.sh
+++ ci/run_flutter_integration_test_android.sh
@@ -20,11 +20,11 @@ curl -L "${IRIS_CDN_URL_ANDROID}" > ${ARTIFACTS_PATH}/${DOWNLOAD_NAME}
 
 VERSION="$(cut -d'_' -f2 <<<"${DOWNLOAD_NAME}")"
 
-pushd ${ARTIFACTS_PATH}/${DOWNLOAD_NAME}
+pushd ${ARTIFACTS_PATH}
     unzip ${DOWNLOAD_NAME} -d ${DOWNLOAD_BASE_NAME}
 popd
 
-UNZIP_PATH="${${ARTIFACTS_PATH}}/${DOWNLOAD_BASE_NAME}/iris_${VERSION}_DCG_Android"
+UNZIP_PATH="${ARTIFACTS_PATH}/${DOWNLOAD_BASE_NAME}/iris_${VERSION}_DCG_Android"
 
 ABIS="arm64-v8a armeabi-v7a x86_64"
 for ABI in ${ABIS};
@@ -36,21 +36,21 @@ do
     cp -RP "${UNZIP_PATH}/ALL_ARCHITECTURE/Release/${ABI}/libIrisDebugger.so" "${IRIS_TESTER_PATH}/android/libs/${ABI}/libIrisDebugger.so"
 done;
 
-# pushd ${MY_PATH}/../test_shard/fake_test_app
+pushd ${MY_PATH}/../test_shard/fake_test_app
 
-# flutter packages get
+flutter packages get
 
-# flutter test integration_test
+flutter test integration_test
 
-# popd
+popd
 
-# pushd ${MY_PATH}/../test_shard/integration_test_app
+pushd ${MY_PATH}/../test_shard/integration_test_app
 
-# flutter packages get
+flutter packages get
 
-# flutter build macos --dart-define=TEST_APP_ID="${TEST_APP_ID}" lib/fake_remote_user_main.dart
-# open build/macos/Build/Products/Release/integration_test_app.app
+flutter build macos --dart-define=TEST_APP_ID="${TEST_APP_ID}" lib/fake_remote_user_main.dart
+open build/macos/Build/Products/Release/integration_test_app.app
 
-# flutter test integration_test --dart-define=TEST_APP_ID="${TEST_APP_ID}"
+flutter test integration_test --dart-define=TEST_APP_ID="${TEST_APP_ID}"
 
-# popd
+popd
