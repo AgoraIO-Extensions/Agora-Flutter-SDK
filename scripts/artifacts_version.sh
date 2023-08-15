b3bf94793c0af8fae1e9e4a49672de6a61bbb357
littleGnAl <littleGnAl@users.noreply.github.com>
2023-08-15 07:09:36 +0000

    feat: upgrade native sdk dependencies 20230815

--- scripts/artifacts_version.sh
+++ scripts/artifacts_version.sh
@@ -1,6 +1,6 @@
 set -e
 
-export IRIS_CDN_URL_ANDROID="https://download.agora.io/sdk/release/iris_4.1.1.147-build.3_DCG_Android_Video_20230811_0336.zip"
-export IRIS_CDN_URL_IOS="https://download.agora.io/sdk/release/iris_4.1.1.147-build.3_DCG_iOS_Video_20230811_0336.zip"
-export IRIS_CDN_URL_MACOS="https://download.agora.io/sdk/release/iris_4.1.1.147-build.3_DCG_Mac_Video_20230811_0336.zip"
-export IRIS_CDN_URL_WINDOWS="https://download.agora.io/sdk/release/iris_4.1.1.147-build.3_DCG_Windows_Video_20230811_0336.zip"
+export IRIS_CDN_URL_ANDROID="https://download.agora.io/sdk/release/iris_4.1.1.147-build.4_DCG_Android_Video_20230815_0248.zip"
+export IRIS_CDN_URL_IOS="https://download.agora.io/sdk/release/iris_4.1.1.147-build.4_DCG_iOS_Video_20230815_0248.zip"
+export IRIS_CDN_URL_MACOS="https://download.agora.io/sdk/release/iris_4.1.1.147-build.4_DCG_Mac_Video_20230815_0248.zip"
+export IRIS_CDN_URL_WINDOWS="https://download.agora.io/sdk/release/iris_4.1.1.147-build.4_DCG_Windows_Video_20230815_0248.zip"
