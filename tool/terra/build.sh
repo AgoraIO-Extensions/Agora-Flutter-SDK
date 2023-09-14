d3f008c29bdbac8f006aa3fc747d0b9bbc9f4d66
littleGnAl <tearjeaker@hotmail.com>
2023-09-14 19:27:21 +0800

    ++

--- tool/terra/build.sh
+++ tool/terra/build.sh
@@ -2,15 +2,19 @@
 set -e
 set -x
 
-TERRA_PATH=$1
+# TERRA_PATH=$1
 MY_PATH=$(realpath $(dirname "$0"))
 PROJECT_ROOT=$(realpath ${MY_PATH}/../..)
 
-pushd $TERRA_PATH
+# pushd $TERRA_PATH
 
-npm run build -- render-legacy \
+# npm run terra -- render \
+#     --config ${PROJECT_ROOT}/tool/terra/terra_config_main.yaml  \
+#     --output-dir=${PROJECT_ROOT}/lib/src \
+#     --export-file-path=${PROJECT_ROOT}/lib/agora_rtc_engine.dart
+
+# popd
+
+npm exec terra -- render \
     --config ${PROJECT_ROOT}/tool/terra/terra_config_main.yaml  \
-    --output-dir=${PROJECT_ROOT}/lib/src \
-    --export-file-path=${PROJECT_ROOT}/lib/agora_rtc_engine.dart
-
-popd
+    --output-dir=${PROJECT_ROOT}/lib/src
