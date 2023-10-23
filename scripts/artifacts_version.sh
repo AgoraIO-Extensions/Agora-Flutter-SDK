ff7a6ceb5424e1da3ffd8a22bb6241aa1ba1bcde
Littlegnal <8847263+littleGnAl@users.noreply.github.com>
2023-10-23 11:54:18 +0800

    fix: [iOS/macOS] Fix CFBundleShortVersionString not correct in AgoraRtcWrapper framework (#1387)
    The `CFBundleShortVersionString` is not correct in `AgoraRtcWrapper`
    frameworks, cause the user can not submit the APP to the APP Store,
    which affects the iOS/macOS.
    
    - Update `s.dependency 'AgoraIrisRTC_iOS', '4.2.3-build.4'`
    - Update `s.dependency 'AgoraIrisRTC_macOS', '4.2.3-build.4'`
    
    Fix https://github.com/AgoraIO-Extensions/Agora-Flutter-SDK/issues/1375
    
    Co-authored-by: littleGnAl <littleGnAl@users.noreply.github.com>

--- scripts/artifacts_version.sh
+++ scripts/artifacts_version.sh
@@ -1,6 +1,6 @@
 set -e
 
 export IRIS_CDN_URL_ANDROID="https://download.agora.io/sdk/release/iris_4.2.3-build.3_DCG_Android_Video_20231012_0417.zip"
-export IRIS_CDN_URL_IOS="https://download.agora.io/sdk/release/iris_4.2.3-build.3_DCG_iOS_Video_20231012_0456.zip"
-export IRIS_CDN_URL_MACOS="https://download.agora.io/sdk/release/iris_4.2.3-build.3_DCG_Mac_Video_20231012_0417.zip"
+export IRIS_CDN_URL_IOS="https://download.agora.io/sdk/release/iris_4.2.3-build.4_DCG_iOS_Video_20231019_0355.zip"
+export IRIS_CDN_URL_MACOS="https://download.agora.io/sdk/release/iris_4.2.3-build.4_DCG_Mac_Video_20231019_0355.zip"
 export IRIS_CDN_URL_WINDOWS="https://download.agora.io/sdk/release/iris_4.2.3-build.3_DCG_Windows_Video_20231012_0417.zip"
