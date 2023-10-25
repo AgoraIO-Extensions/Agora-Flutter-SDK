5e4599b033a6acbab145e130875369ed4e8404a9
littleGnAl <tearjeaker@hotmail.com>
2023-10-25 18:55:25 +0800

    ++

---
+++ example/lib/components/config_override.dart
@@ -0,0 +1,48 @@
+/// Key of APP ID
+const keyAppId = 'TEST_APP_ID';
+
+/// Key of Channel ID
+const keyChannelId = 'TEST_CHANNEL_ID';
+
+/// Key of token
+const keyToken = 'TEST_TOKEN';
+
+ExampleConfigOverride? _gConfigOverride;
+
+/// This class allow override the config(appId/channelId/token) in the example.
+class ExampleConfigOverride {
+  ExampleConfigOverride._();
+
+  factory ExampleConfigOverride() {
+    _gConfigOverride = _gConfigOverride ?? ExampleConfigOverride._();
+    return _gConfigOverride!;
+  }
+  final Map<String, String> _overridedConfig = {};
+
+  /// Get the expected APP ID
+  String getAppId() {
+    return _overridedConfig[keyAppId] ??
+        // Allow pass an `appId` as an environment variable with name `TEST_APP_ID` by using --dart-define
+        const String.fromEnvironment(keyAppId, defaultValue: '<TEST_APP_ID>');
+  }
+
+  /// Get the expected Channel ID
+  String getChannelId() {
+    return _overridedConfig[keyChannelId] ??
+        // Allow pass a `token` as an environment variable with name `TEST_TOKEN` by using --dart-define
+        const String.fromEnvironment(keyChannelId,
+            defaultValue: '<TEST_CHANNEL_ID>');
+  }
+
+  /// Get the expected Token
+  String getToken() {
+    return _overridedConfig[keyToken] ??
+        // Allow pass a `channelId` as an environment variable with name `TEST_CHANNEL_ID` by using --dart-define
+        const String.fromEnvironment(keyToken, defaultValue: '<TEST_TOKEN>');
+  }
+
+  /// Override the config(appId/channelId/token)
+  void set(String name, String value) {
+    _overridedConfig[name] = value;
+  }
+}
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:agora_rtc_engine_example/components/android_foreground_service_widget.dart';
+import 'package:agora_rtc_engine_example/components/config_override.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:permission_handler/permission_handler.dart';
@@ -26,6 +27,8 @@ class _MyAppState extends State<MyApp> {
 
   bool _showPerformanceOverlay = false;
 
+  bool _isWebSetup = false;
+
   bool _isConfigInvalid() {
     return config.appId == '<YOUR_APP_ID>' ||
         config.token == '<YOUR_TOKEN>' ||
@@ -35,6 +38,8 @@ class _MyAppState extends State<MyApp> {
   @override
   void initState() {
     super.initState();
+
+    _isWebSetup = !kIsWeb;
 
     _requestPermissionIfNeed();
   }
@@ -74,51 +79,157 @@ class _MyAppState extends State<MyApp> {
             )
           ],
         ),
-        body: _isConfigInvalid()
-            ? const InvalidConfigWidget()
-            : ListView.builder(
-                itemCount: _data.length,
-                itemBuilder: (context, index) {
-                  return _data[index]['widget'] == null
-                      ? Ink(
-                          color: Colors.grey,
-                          child: ListTile(
-                            title: Text(_data[index]['name'] as String,
-                                style: const TextStyle(
-                                    fontSize: 24, color: Colors.white)),
-                          ),
-                        )
-                      : ListTile(
-                          onTap: () {
-                            Navigator.push(context,
-                                MaterialPageRoute(builder: (context) {
-                              Widget widget = Scaffold(
-                                appBar: AppBar(
-                                  title: Text(_data[index]['name'] as String),
-                                  // ignore: prefer_const_literals_to_create_immutables
-                                  actions: [const LogActionWidget()],
-                                ),
-                                body: _data[index]['widget'] as Widget?,
-                              );
-
-                              if (!kIsWeb && Platform.isAndroid) {
-                                widget = AndroidForegroundServiceWidget(
-                                    child: widget);
-                              }
-
-                              return widget;
-                            }));
-                          },
-                          title: Text(
-                            _data[index]['name'] as String,
-                            style: const TextStyle(
-                                fontSize: 24, color: Colors.black),
-                          ),
-                        );
-                },
-              ),
+        body: _body(),
       ),
     );
+  }
+
+  Widget _body() {
+    if (!_isWebSetup) {
+      return _WebSetupPage(setupCompleted: () {
+        setState(() {
+          _isWebSetup = true;
+        });
+      });
+    }
+
+    if (_isConfigInvalid()) {
+      return const InvalidConfigWidget();
+    }
+
+    return ListView.builder(
+      itemCount: _data.length,
+      itemBuilder: (context, index) {
+        return _data[index]['widget'] == null
+            ? Ink(
+                color: Colors.grey,
+                child: ListTile(
+                  title: Text(_data[index]['name'] as String,
+                      style:
+                          const TextStyle(fontSize: 24, color: Colors.white)),
+                ),
+              )
+            : ListTile(
+                onTap: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) {
+                    Widget widget = Scaffold(
+                      appBar: AppBar(
+                        title: Text(_data[index]['name'] as String),
+                        // ignore: prefer_const_literals_to_create_immutables
+                        actions: [const LogActionWidget()],
+                      ),
+                      body: _data[index]['widget'] as Widget?,
+                    );
+
+                    if (!kIsWeb && Platform.isAndroid) {
+                      widget = AndroidForegroundServiceWidget(child: widget);
+                    }
+
+                    return widget;
+                  }));
+                },
+                title: Text(
+                  _data[index]['name'] as String,
+                  style: const TextStyle(fontSize: 24, color: Colors.black),
+                ),
+              );
+      },
+    );
+  }
+}
+
+class _WebSetupPage extends StatefulWidget {
+  const _WebSetupPage({Key? key, required this.setupCompleted})
+      : super(key: key);
+
+  final VoidCallback setupCompleted;
+
+  @override
+  State<_WebSetupPage> createState() => _WebSetupPageState();
+}
+
+class _WebSetupPageState extends State<_WebSetupPage> {
+  late TextEditingController _appIdController;
+  late TextEditingController _channelIdController;
+  late TextEditingController _tokenController;
+
+  bool _isValid = false;
+
+  late final ExampleConfigOverride _configOverride;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _configOverride = ExampleConfigOverride();
+
+    _appIdController = TextEditingController(text: _configOverride.getAppId());
+    _channelIdController =
+        TextEditingController(text: _configOverride.getChannelId());
+    _tokenController = TextEditingController(text: _configOverride.getToken());
+
+    _appIdController.addListener(_validCheck);
+    _channelIdController.addListener(_validCheck);
+  }
+
+  void _validCheck() {
+    _isValid = _appIdController.text.isNotEmpty &&
+        _channelIdController.text.isNotEmpty;
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: SizedBox(
+        width: 300,
+        child: Column(
+          children: [
+            const Text('Input Your APP ID'),
+            TextField(
+              controller: _appIdController,
+              decoration: const InputDecoration(
+                labelText: 'APP ID can not be empty',
+                // errorText: _appIdValidate ? "Value Can't Be Empty" : null,
+              ),
+            ),
+            const Text('Input Your Channel ID'),
+            TextField(
+              controller: _channelIdController,
+              decoration: const InputDecoration(
+                labelText: 'Channel ID can not be empty',
+                // errorText: _appIdValidate ? "Value Can't Be Empty" : null,
+              ),
+            ),
+            const Text('Input Your Token (Optional)'),
+            TextField(
+              controller: _tokenController,
+            ),
+            ElevatedButton(
+              onPressed: !_isValid
+                  ? null
+                  : () {
+                      _configOverride.set(keyAppId, _appIdController.text);
+                      _configOverride.set(
+                          keyChannelId, _channelIdController.text);
+                      _configOverride.set(keyToken, _tokenController.text);
+
+                      widget.setupCompleted();
+                    },
+              child: const Text('Done'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _appIdController.dispose();
+    _channelIdController.dispose();
+    _tokenController.dispose();
+    super.dispose();
   }
 }
 
