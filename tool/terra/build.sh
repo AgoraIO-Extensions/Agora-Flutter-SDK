4e3d32a9fe6bef6a48fdaa183357154b93b68d25
littleGnAl <tearjeaker@hotmail.com>
2023-10-30 13:22:24 +0800

    [tool] Migrate to the new terra

--- tool/terra/build.sh
+++ tool/terra/build.sh
@@ -2,15 +2,9 @@
 set -e
 set -x
 
-TERRA_PATH=$1
 MY_PATH=$(realpath $(dirname "$0"))
 PROJECT_ROOT=$(realpath ${MY_PATH}/../..)
 
-pushd $TERRA_PATH
-
-npm run build -- render-legacy \
+npm exec terra -- run \
     --config ${PROJECT_ROOT}/tool/terra/terra_config_main.yaml  \
-    --output-dir=${PROJECT_ROOT}/lib/src \
-    --export-file-path=${PROJECT_ROOT}/lib/agora_rtc_engine.dart
-
-popd
+    --output-dir=${PROJECT_ROOT}/lib/src
