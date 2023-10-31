30de17c63e31af0629cde828bc993fe5a1499d78
littleGnAl <tearjeaker@hotmail.com>
2023-10-31 12:09:11 +0800

    ++

--- tool/terra/build.sh
+++ tool/terra/build.sh
@@ -5,6 +5,10 @@ set -x
 MY_PATH=$(realpath $(dirname "$0"))
 PROJECT_ROOT=$(realpath ${MY_PATH}/../..)
 
+pushd ${MY_PATH}
+
 npm exec terra -- run \
     --config ${PROJECT_ROOT}/tool/terra/terra_config_main.yaml  \
     --output-dir=${PROJECT_ROOT}/lib/src
+
+popd
