64542d1e3512b9170a7095a9f629712ae49c926b
littleGnAl <littleGnAl@users.noreply.github.com>
2023-11-24 06:47:43 +0000

    feat: upgrade native sdk 4.2.6.1

--- scripts/artifacts_version.sh
+++ scripts/artifacts_version.sh
@@ -2,5 +2,5 @@ set -e
 
 export IRIS_CDN_URL_ANDROID="https://download.agora.io/sdk/release/iris_4.2.3.2-build.1_DCG_Android_Video_20231115_0251.zip"
 export IRIS_CDN_URL_IOS="https://download.agora.io/sdk/release/iris_4.2.3.2-build.1_DCG_iOS_Video_20231115_0251.zip"
-export IRIS_CDN_URL_MACOS="https://download.agora.io/sdk/release/iris_4.2.3.2-build.1_DCG_Mac_Video_20231115_0251.zip"
-export IRIS_CDN_URL_WINDOWS="https://download.agora.io/sdk/release/iris_4.2.3.2-build.1_DCG_Windows_Video_20231115_0251.zip"
+export IRIS_CDN_URL_MACOS="https://download.agora.io/sdk/release/iris_4.2.6.1-build.1_DCG_Mac_Video_20231123_0423.zip"
+export IRIS_CDN_URL_WINDOWS="https://download.agora.io/sdk/release/iris_4.2.6.1-build.1_DCG_Windows_Video_20231123_0423.zip"
