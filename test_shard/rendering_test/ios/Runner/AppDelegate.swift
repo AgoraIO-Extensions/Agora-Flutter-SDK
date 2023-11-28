43cf55c9d69f918dae0dcea3707503a5a6d91f12
Littlegnal <8847263+littleGnAl@users.noreply.github.com>
2023-11-28 08:27:45 +0000

    ++

--- test_shard/rendering_test/ios/Runner/AppDelegate.swift
+++ test_shard/rendering_test/ios/Runner/AppDelegate.swift
@@ -11,8 +11,6 @@ import integration_test
     GeneratedPluginRegistrant.register(with: self)
       
     let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
-
-    IntegrationTestPlugin.instance().setupChannels(controller.binaryMessenger)
       
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
