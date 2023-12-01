0c57a7b30bee4696a777991ca14dd52614b42825
LichKing-2234 <LichKing-2234@users.noreply.github.com>
2023-12-01 04:19:25 +0000

    feat: upgrade native sdk 4.1.1.21

--- scripts/artifacts_version.sh
+++ scripts/artifacts_version.sh
@@ -1,6 +1,6 @@
 set -e
 
-export IRIS_CDN_URL_ANDROID="https://download.agora.io/sdk/release/iris_4.1.1.21-banban.1_DCG_Android_Video_20231114_0339.zip"
-export IRIS_CDN_URL_IOS="https://download.agora.io/sdk/release/iris_4.1.1.21-banban.1_DCG_iOS_Video_20231114_0339.zip"
-export IRIS_CDN_URL_MACOS="https://download.agora.io/sdk/release/iris_4.1.1.21-banban.1_DCG_Mac_Video_20231114_0339.zip"
-export IRIS_CDN_URL_WINDOWS="https://download.agora.io/sdk/release/iris_4.1.1.21-banban.1_DCG_Windows_Video_20231114_0339.zip"
+export IRIS_CDN_URL_ANDROID="https://download.agora.io/sdk/release/iris_4.1.1.21-banban.6_DCG_Android_Video_20231201_1205.zip"
+export IRIS_CDN_URL_IOS="https://download.agora.io/sdk/release/iris_4.1.1.21-banban.6_DCG_iOS_Video_20231201_1205.zip"
+export IRIS_CDN_URL_MACOS="https://download.agora.io/sdk/release/iris_4.1.1.21-banban.6_DCG_Mac_Video_20231201_1205.zip"
+export IRIS_CDN_URL_WINDOWS="https://download.agora.io/sdk/release/iris_4.1.1.21-banban.6_DCG_Windows_Video_20231201_1205.zip"
