885e975c83a9310b80d28056e37e955b21d675e7
Littlegnal <8847263+littleGnAl@users.noreply.github.com>
2024-01-22 11:09:56 +0800

    feat: upgrade native sdk dependencies 20240122 (#1516)
    Update native sdk dependencies 20240122
    native sdk dependencies:
    ```
    打包助手 1-19 19:21 Packages: implementation 'io.agora.rtc:agora-full-preview:4.3.0-dev.18'  打包助手 1-19 19:21 Packages: implementation 'io.agora.rtc:full-screen-sharing-special:4.3.0-dev.18'  打包助手 1-19 19:21 Packages: pod 'AgoraRtcEngine_macOS_Preview', '4.3.0-dev.18'  打包助手 1-19 19:22 Packages: pod 'AgoraRtcEngine_iOS_Preview', '4.3.0-dev.18'
    ```
    
    iris dependencies:
    ```
    打包助手 1-19 19:10 Iris: Artifactory: https://artifactory-api.bj2.agoralab.co/artifactory/CSDC_repo/EP/4.3/20240119/ios/iris_4.3.0-dev.18_DCG_iOS_Video_20240119_0702.zip PrivateArtifactory: https://artifactory-api.bj2.agoralab.co/artifactory/CSDC_repo/EP/4.3/20240119/ios/iris_4.3.0-dev.18_DCG_iOS_Video_20240119_0702_private.zip CDN: https://download.agora.io/sdk/release/iris_4.3.0-dev.18_DCG_iOS_Video_20240119_0702.zip Cocoapods: pod 'AgoraIrisRTC_iOS', '4.3.0-dev.18'  打包助手 1-19 19:15 Iris: Artifactory: https://artifactory-api.bj2.agoralab.co/artifactory/CSDC_repo/EP/4.3/20240119/mac/iris_4.3.0-dev.18_DCG_Mac_Video_20240119_0702.zip PrivateArtifactory: https://artifactory-api.bj2.agoralab.co/artifactory/CSDC_repo/EP/4.3/20240119/mac/iris_4.3.0-dev.18_DCG_Mac_Video_20240119_0702_private.zip CDN: https://download.agora.io/sdk/release/iris_4.3.0-dev.18_DCG_Mac_Video_20240119_0702.zip Cocoapods: pod 'AgoraIrisRTC_macOS', '4.3.0-dev.18'  打包助手 1-19 19:19 Iris: Artifactory: https://artifactory-api.bj2.agoralab.co/artifactory/CSDC_repo/EP/4.3/20240119/windows/iris_4.3.0-dev.18_DCG_Windows_Video_20240119_0702.zip Private: https://artifactory-api.bj2.agoralab.co/artifactory/CSDC_repo/EP/4.3/20240119/windows/iris_4.3.0-dev.18_DCG_Windows_Video_20240119_0702_private.zip CDN: https://download.agora.io/sdk/release/iris_4.3.0-dev.18_DCG_Windows_Video_20240119_0702.zip Packages: implementation 'io.agora.rtc:iris-rtc:4.3.0-dev.18'
    ```
    
    > This pull request is trigger by bot, DO NOT MODIFY BY HAND.
    
    Co-authored-by: littleGnAl <littleGnAl@users.noreply.github.com>

--- scripts/artifacts_version.sh
+++ scripts/artifacts_version.sh
@@ -1,6 +1,6 @@
 set -e
 
 export IRIS_CDN_URL_ANDROID="https://download.agora.io/sdk/release/iris_4.3.0-dev.16_DCG_Android_Video_20240102_1139.zip"
-export IRIS_CDN_URL_IOS="https://download.agora.io/sdk/release/iris_4.3.0-test.1_DCG_iOS_Video_20240119_0338.zip"
-export IRIS_CDN_URL_MACOS="https://download.agora.io/sdk/release/iris_4.3.0-test.1_DCG_Mac_Video_20240119_0338.zip"
-export IRIS_CDN_URL_WINDOWS="https://download.agora.io/sdk/release/iris_4.3.0-test.1_DCG_Windows_Video_20240119_0338.zip"
+export IRIS_CDN_URL_IOS="https://download.agora.io/sdk/release/iris_4.3.0-dev.18_DCG_iOS_Video_20240119_0702.zip"
+export IRIS_CDN_URL_MACOS="https://download.agora.io/sdk/release/iris_4.3.0-dev.18_DCG_Mac_Video_20240119_0702.zip"
+export IRIS_CDN_URL_WINDOWS="https://download.agora.io/sdk/release/iris_4.3.0-dev.18_DCG_Windows_Video_20240119_0702.zip"
