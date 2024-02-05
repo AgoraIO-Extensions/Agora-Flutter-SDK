8f818c23920012d17fa1b699b76fd935c5295ac3
Littlegnal <8847263+littleGnAl@users.noreply.github.com>
2024-02-05 10:52:32 +0800

    fix: [texture rendering] Fix texture id lost when widget is updated (#1543)
    We get the texture id from the `VideoViewController` that is passed by
    the user directly, which will be reset if the widget is updated, so we
    need to maintain it internally.

--- test_shard/integration_test_app/integration_test/testcases/fake_agora_video_view_testcases.dart
+++ test_shard/integration_test_app/integration_test/testcases/fake_agora_video_view_testcases.dart
@@ -504,6 +504,401 @@ void testCases() {
       skip: !(Platform.isAndroid || Platform.isIOS),
     );
 
+    group(
+      'Texture Rendering',
+      () {
+        testWidgets(
+          'Show local AgoraVideoView after RtcEngine.initialize',
+          (WidgetTester tester) async {
+            late FakeMethodChannelController fakeMethodChannelController;
+            fakeMethodChannelController = FakeMethodChannelController(
+              rtcEngine,
+              tester.binding.defaultBinaryMessenger,
+            );
+
+            final videoViewCreatedCompleter = Completer<void>();
+
+            await tester.pumpWidget(_RenderViewWidget(
+              rtcEngine: rtcEngine,
+              builder: (context, engine) {
+                return SizedBox(
+                  height: 100,
+                  width: 100,
+                  child: AgoraVideoView(
+                    controller: VideoViewController(
+                      rtcEngine: engine,
+                      canvas: const VideoCanvas(uid: 0),
+                      useFlutterTexture: true,
+                    ),
+                    onAgoraVideoViewCreated: (viewId) {
+                      if (!videoViewCreatedCompleter.isCompleted) {
+                        videoViewCreatedCompleter.complete(null);
+                      }
+                    },
+                  ),
+                );
+              },
+            ));
+
+            await tester.pumpAndSettle(const Duration(milliseconds: 5000));
+            // pumpAndSettle again to ensure the `AgoraVideoView` shown
+            await tester.pumpAndSettle(const Duration(milliseconds: 5000));
+
+            await videoViewCreatedCompleter.future;
+
+            {
+              final createTextureRenderCalls = fakeMethodChannelController
+                  .methodCallQueue
+                  .where((e) => e.method == 'createTextureRender')
+                  .toList();
+
+              final createTextureRenderArg = Map<String, Object>.from(
+                  createTextureRenderCalls[0].arguments);
+
+              expect(createTextureRenderArg['uid'] == 0, isTrue);
+
+              int textureId = -1;
+              expect(find.byWidgetPredicate((widget) {
+                final found = widget is Texture;
+                if (found) {
+                  textureId = (widget as Texture).textureId;
+                }
+                return found;
+              }), findsOneWidget);
+              // The first textureId is 0
+              expect(textureId == 0, isTrue);
+            }
+
+            fakeMethodChannelController.reset();
+
+            await tester.pumpWidget(Container());
+            await tester.pumpAndSettle(const Duration(milliseconds: 5000));
+
+            // Delay 5 seconds to ensure that previous Widget.dipose call completed.
+            await Future.delayed(const Duration(seconds: 5));
+
+            {
+              final disposeTextureRenderCalls = fakeMethodChannelController
+                  .methodCallQueue
+                  .where((e) => e.method == 'destroyTextureRender')
+                  .toList();
+
+              // texture id
+              final disposeTextureRenderTextureId =
+                  disposeTextureRenderCalls[0].arguments as int;
+
+              expect(disposeTextureRenderTextureId != -1, isTrue);
+            }
+
+            fakeMethodChannelController.dispose();
+          },
+        );
+
+        testWidgets(
+          'Show local AgoraVideoView after udpate the widget multiple times',
+          (WidgetTester tester) async {
+            final fakeMethodChannelController = FakeMethodChannelController(
+              rtcEngine,
+              tester.binding.defaultBinaryMessenger,
+            );
+
+            Completer<void> videoViewCreatedCompleter = Completer<void>();
+
+            await tester.pumpWidget(_RenderViewWidget(
+              rtcEngine: rtcEngine,
+              builder: (context, engine) {
+                return SizedBox(
+                  height: 100,
+                  width: 100,
+                  child: AgoraVideoView(
+                    controller: VideoViewController(
+                      rtcEngine: engine,
+                      canvas: const VideoCanvas(uid: 0),
+                      useFlutterTexture: true,
+                    ),
+                    onAgoraVideoViewCreated: (viewId) {
+                      if (!videoViewCreatedCompleter.isCompleted) {
+                        videoViewCreatedCompleter.complete(null);
+                      }
+                    },
+                  ),
+                );
+              },
+            ));
+
+            await tester.pumpAndSettle(const Duration(milliseconds: 5000));
+            // pumpAndSettle again to ensure the `AgoraVideoView` shown
+            await tester.pumpAndSettle(const Duration(milliseconds: 5000));
+
+            await videoViewCreatedCompleter.future;
+
+            {
+              int textureId = -1;
+              expect(find.byWidgetPredicate((widget) {
+                final found = widget is Texture;
+                if (found) {
+                  textureId = (widget as Texture).textureId;
+                }
+                return found;
+              }), findsOneWidget);
+              // The first textureId is 0
+              expect(textureId == 0, isTrue);
+            }
+
+            videoViewCreatedCompleter = Completer<void>();
+            await tester.pumpWidget(_RenderViewWidget(
+              rtcEngine: rtcEngine,
+              builder: (context, engine) {
+                return SizedBox(
+                  height: 100,
+                  width: 100,
+                  child: AgoraVideoView(
+                    controller: VideoViewController(
+                      rtcEngine: engine,
+                      canvas: const VideoCanvas(uid: 0),
+                      useFlutterTexture: true,
+                    ),
+                    onAgoraVideoViewCreated: (viewId) {
+                      if (!videoViewCreatedCompleter.isCompleted) {
+                        videoViewCreatedCompleter.complete(null);
+                      }
+                    },
+                  ),
+                );
+              },
+            ));
+
+            await tester.pumpAndSettle(const Duration(milliseconds: 5000));
+            // pumpAndSettle again to ensure the `AgoraVideoView` shown
+            await tester.pumpAndSettle(const Duration(milliseconds: 5000));
+
+            {
+              final createTextureRenderCalls = fakeMethodChannelController
+                  .methodCallQueue
+                  .where((e) => e.method == 'createTextureRender')
+                  .toList();
+              expect(createTextureRenderCalls.length == 1, isTrue);
+
+              int textureId = -1;
+              expect(find.byWidgetPredicate((widget) {
+                final found = widget is Texture;
+                if (found) {
+                  textureId = (widget as Texture).textureId;
+                }
+                return found;
+              }), findsOneWidget);
+              // The first textureId is 0
+              expect(textureId == 0, isTrue);
+            }
+
+            fakeMethodChannelController.dispose();
+          },
+        );
+
+        testWidgets(
+          'Switch local/remote AgoraVideoView with RtcConnection',
+          (WidgetTester tester) async {
+            // This case run this following steps:
+            // 1. Show a local `AgoraVideoView`.
+            // 2. Show local and remote `AgoraVideoView`, this step will trigger the `State.didUpdateWidget`.
+            // 3. Switch the local and remote `AgoraVideoView`.
+
+            final fakeMethodChannelController = FakeMethodChannelController(
+              rtcEngine,
+              tester.binding.defaultBinaryMessenger,
+            );
+
+            await tester.pumpWidget(_RenderViewWidget(
+              rtcEngine: rtcEngine,
+              builder: (context, engine) {
+                return Column(
+                  children: [
+                    SizedBox(
+                      height: 100,
+                      width: 100,
+                      child: AgoraVideoView(
+                        controller: VideoViewController(
+                          rtcEngine: engine,
+                          canvas: const VideoCanvas(uid: 0),
+                          useFlutterTexture: true,
+                        ),
+                      ),
+                    )
+                  ],
+                );
+              },
+            ));
+
+            await tester.pumpAndSettle(const Duration(milliseconds: 5000));
+            // pumpAndSettle again to ensure the `AgoraVideoView` shown
+            await tester.pumpAndSettle(const Duration(milliseconds: 5000));
+
+            // Check `GlobalVideoViewController.createTextureRender` calls
+            {
+              final createTextureRenderCalls = fakeMethodChannelController
+                  .methodCallQueue
+                  .where((e) => e.method == 'createTextureRender')
+                  .toList();
+
+              final createTextureRenderArg = Map<String, Object>.from(
+                  createTextureRenderCalls[0].arguments);
+
+              expect(createTextureRenderArg['uid'] == 0, isTrue);
+            }
+
+            // This step will call `didUpdateWidget`
+            await tester.pumpWidget(_RenderViewWidget(
+              rtcEngine: rtcEngine,
+              builder: (context, engine) {
+                return Column(
+                  children: [
+                    SizedBox(
+                      height: 100,
+                      width: 100,
+                      child: AgoraVideoView(
+                        controller: VideoViewController(
+                          rtcEngine: engine,
+                          canvas: const VideoCanvas(uid: 0),
+                          useFlutterTexture: true,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 100,
+                      width: 100,
+                      child: AgoraVideoView(
+                        controller: VideoViewController.remote(
+                          rtcEngine: engine,
+                          canvas: const VideoCanvas(uid: 1000),
+                          connection: const RtcConnection(
+                            channelId: 'switch_video_view',
+                            localUid: 1000,
+                          ),
+                          useFlutterTexture: true,
+                        ),
+                      ),
+                    )
+                  ],
+                );
+              },
+            ));
+
+            await tester.pumpAndSettle(const Duration(milliseconds: 5000));
+            // pumpAndSettle again to ensure the `AgoraVideoView` shown
+            await tester.pumpAndSettle(const Duration(milliseconds: 5000));
+
+            // Check `GlobalVideoViewController.createTextureRender` calls for remote `AgoraVideoView`
+            {
+              final createTextureRenderCalls = fakeMethodChannelController
+                  .methodCallQueue
+                  .where((e) => e.method == 'createTextureRender')
+                  .toList();
+
+              expect(createTextureRenderCalls.length == 2, isTrue);
+
+              final createTextureRenderArg = Map<String, Object>.from(
+                  createTextureRenderCalls[1].arguments);
+
+              expect(createTextureRenderArg['uid'] != 0, isTrue);
+
+              {
+                List<int> textureIds = [];
+                expect(find.byWidgetPredicate((widget) {
+                  final found = widget is Texture;
+                  if (found) {
+                    // textureId = (widget as Texture).textureId;
+                    textureIds.add((widget as Texture).textureId);
+                  }
+                  return found;
+                }), findsNWidgets(2));
+                // The first textureId is 0
+                expect(textureIds[0] == 0, isTrue);
+                // The second textureId is 0
+                expect(textureIds[1] == 1, isTrue);
+              }
+            }
+
+            await tester.pumpWidget(_RenderViewWidget(
+              rtcEngine: rtcEngine,
+              builder: (context, engine) {
+                return Column(
+                  children: [
+                    SizedBox(
+                      height: 100,
+                      width: 100,
+                      child: AgoraVideoView(
+                        controller: VideoViewController.remote(
+                          rtcEngine: engine,
+                          canvas: const VideoCanvas(uid: 1000),
+                          connection: const RtcConnection(
+                            channelId: 'switch_video_view',
+                            localUid: 1000,
+                          ),
+                          useFlutterTexture: true,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 100,
+                      width: 100,
+                      child: AgoraVideoView(
+                        controller: VideoViewController(
+                          rtcEngine: engine,
+                          canvas: const VideoCanvas(uid: 0),
+                          useFlutterTexture: true,
+                        ),
+                      ),
+                    )
+                  ],
+                );
+              },
+            ));
+
+            await tester.pumpAndSettle(const Duration(milliseconds: 5000));
+
+            {
+              final createTextureRenderCalls = fakeMethodChannelController
+                  .methodCallQueue
+                  .where((e) => e.method == 'createTextureRender')
+                  .toList();
+
+              // After `didUpdateWidget` called, the texture renderer will be re-created,
+              // so there're another 2 times' `createTextureRender` are called.
+              // So the value of `createTextureRenderCalls.length` is 4
+              expect(createTextureRenderCalls.length == 4, isTrue);
+
+              final createTextureRenderArg = Map<String, Object>.from(
+                  createTextureRenderCalls[2].arguments);
+
+              expect(createTextureRenderArg['uid'] != 0, isTrue);
+
+              {
+                List<int> textureIds = [];
+                expect(find.byWidgetPredicate((widget) {
+                  final found = widget is Texture;
+                  if (found) {
+                    // textureId = (widget as Texture).textureId;
+                    textureIds.add((widget as Texture).textureId);
+                  }
+                  return found;
+                }), findsNWidgets(2));
+                // The third textureId is 2
+                expect(textureIds[0] == 2, isTrue);
+                // The fourth textureId is 3
+                expect(textureIds[1] == 3, isTrue);
+              }
+            }
+
+            fakeMethodChannelController.dispose();
+
+            await tester.pumpWidget(Container());
+            await tester.pumpAndSettle(const Duration(milliseconds: 5000));
+            await Future.delayed(const Duration(seconds: 5));
+          },
+        );
+      },
+    );
+
     testWidgets(
       'dispose AgoraVideoView not crash before setupLocalVideo/setupRemoteVideo[Ex] call is completed',
       (WidgetTester tester) async {
