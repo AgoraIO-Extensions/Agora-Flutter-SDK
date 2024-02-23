5c7633596dd33b508148e925d3942950508c03e3
Littlegnal <8847263+littleGnAl@users.noreply.github.com>
2024-02-23 10:45:16 +0800

    [ci] Disable rendering test agora_video_view_render_test.dart  on iOS (#1581)
    See https://github.com/AgoraIO-Extensions/Agora-Flutter-SDK/issues/1580

--- ci/rendering_test_ios.sh
+++ ci/rendering_test_ios.sh
@@ -5,13 +5,16 @@ set -x
 
 MY_PATH=$(dirname "$0")
 
+export SAVE_DEBUG_GOLDEN="true"
+
 pushd ${MY_PATH}/../test_shard/rendering_test
 
 flutter packages get
 
 # TODO(littlegnal): Add `--no-enable-impeller` flag to disable impeller at this time.
 # https://github.com/flutter/flutter/issues/134852
-flutter drive --no-enable-impeller --driver=test_driver/integration_test.dart --target=integration_test/agora_video_view_render_test.dart --dart-define=TEST_APP_ID="${TEST_APP_ID}" --verbose
+# TODO(littlegnal): Temporily disable the `agora_video_view_render_test.dart` https://github.com/AgoraIO-Extensions/Agora-Flutter-SDK/issues/1580
+# flutter drive --no-enable-impeller --driver=test_driver/integration_test.dart --target=integration_test/agora_video_view_render_test.dart --dart-define=TEST_APP_ID="${TEST_APP_ID}" --verbose
 
 flutter drive --no-enable-impeller --driver=test_driver/integration_test.dart --target=integration_test/agora_video_view_smoke_test.dart --dart-define=TEST_APP_ID="${TEST_APP_ID}" --verbose
 
