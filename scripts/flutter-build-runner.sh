453da3d8072481630ecefbf8656250dfe53de81a
littleGnAl <tearjeaker@hotmail.com>
2024-03-15 15:49:00 +0800

    ++

--- scripts/flutter-build-runner.sh
+++ scripts/flutter-build-runner.sh
@@ -7,22 +7,24 @@ AGORA_FLUTTER_PROJECT_PATH=$(realpath ${MY_PATH}/..)
 
 pushd ${AGORA_FLUTTER_PROJECT_PATH}
 
-rm -rf $AGORA_FLUTTER_PROJECT_PATH/example/macos/Flutter/ephemeral
-rm -rf $AGORA_FLUTTER_PROJECT_PATH/example/windows/Flutter/ephemeral
-rm -rf $AGORA_FLUTTER_PROJECT_PATH/example/linux/Flutter/ephemeral
-rm -rf $AGORA_FLUTTER_PROJECT_PATH/example/ios/.symlinks
+if [! ${GITHUB_ACTIONS}]; then
+    rm -rf $AGORA_FLUTTER_PROJECT_PATH/example/macos/Flutter/ephemeral
+    rm -rf $AGORA_FLUTTER_PROJECT_PATH/example/windows/Flutter/ephemeral
+    rm -rf $AGORA_FLUTTER_PROJECT_PATH/example/linux/Flutter/ephemeral
+    rm -rf $AGORA_FLUTTER_PROJECT_PATH/example/ios/.symlinks
 
-rm -rf $AGORA_FLUTTER_PROJECT_PATH/test_shard/fake_test_app/macos/Flutter/ephemeral
-rm -rf $AGORA_FLUTTER_PROJECT_PATH/test_shard/fake_test_app/windows/Flutter/ephemeral
-rm -rf $AGORA_FLUTTER_PROJECT_PATH/test_shard/fake_test_app/ios/.symlinks
+    rm -rf $AGORA_FLUTTER_PROJECT_PATH/test_shard/fake_test_app/macos/Flutter/ephemeral
+    rm -rf $AGORA_FLUTTER_PROJECT_PATH/test_shard/fake_test_app/windows/Flutter/ephemeral
+    rm -rf $AGORA_FLUTTER_PROJECT_PATH/test_shard/fake_test_app/ios/.symlinks
 
-rm -rf $AGORA_FLUTTER_PROJECT_PATH/test_shard/integration_test_app/macos/Flutter/ephemeral
-rm -rf $AGORA_FLUTTER_PROJECT_PATH/test_shard/integration_test_app/windows/Flutter/ephemeral
-rm -rf $AGORA_FLUTTER_PROJECT_PATH/test_shard/integration_test_app/ios/.symlinks
+    rm -rf $AGORA_FLUTTER_PROJECT_PATH/test_shard/integration_test_app/macos/Flutter/ephemeral
+    rm -rf $AGORA_FLUTTER_PROJECT_PATH/test_shard/integration_test_app/windows/Flutter/ephemeral
+    rm -rf $AGORA_FLUTTER_PROJECT_PATH/test_shard/integration_test_app/ios/.symlinks
 
-rm -rf $AGORA_FLUTTER_PROJECT_PATH/test_shard/rendering_test/macos/Flutter/ephemeral
-rm -rf $AGORA_FLUTTER_PROJECT_PATH/test_shard/rendering_test/windows/Flutter/ephemeral
-rm -rf $AGORA_FLUTTER_PROJECT_PATH/test_shard/rendering_test/ios/.symlinks
+    rm -rf $AGORA_FLUTTER_PROJECT_PATH/test_shard/rendering_test/macos/Flutter/ephemeral
+    rm -rf $AGORA_FLUTTER_PROJECT_PATH/test_shard/rendering_test/windows/Flutter/ephemeral
+    rm -rf $AGORA_FLUTTER_PROJECT_PATH/test_shard/rendering_test/ios/.symlinks
+fi
 
 flutter packages pub run build_runner build --delete-conflicting-outputs
 
