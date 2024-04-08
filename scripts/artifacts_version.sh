7d585289204a97a133c3d1ecbdd54a0ee946aef3
Littlegnal <8847263+littleGnAl@users.noreply.github.com>
2024-04-08 16:15:57 +0800

    feat: upgrade native sdk 4.2.6.8.MINI.VIDEO (#1683)
    Update native sdk 4.2.6.8.MINI.VIDEO dependencies
    
    > This pull request is trigger by bot, you can checkout this branch and
    update it.
    
    Co-authored-by: littleGnAl <littleGnAl@users.noreply.github.com>

--- scripts/artifacts_version.sh
+++ scripts/artifacts_version.sh
@@ -1,6 +1,6 @@
 set -e
 
-export IRIS_CDN_URL_ANDROID="https://download.agora.io/sdk/release/iris_4.2.6.6-build.1_DCG_Android_Video_20240206_0333.zip"
-export IRIS_CDN_URL_IOS="https://download.agora.io/sdk/release/iris_4.2.6.6-build.1_DCG_iOS_Video_20240206_0333.zip"
+export IRIS_CDN_URL_ANDROID="https://download.agora.io/sdk/release/iris_4.2.6.8-build.1_DCG_Android_Video_20240327_1207.zip"
+export IRIS_CDN_URL_IOS="https://download.agora.io/sdk/release/iris_4.2.6.8-build.1_DCG_iOS_Video_20240327_1207.zip"
 export IRIS_CDN_URL_MACOS="https://download.agora.io/sdk/release/iris_4.2.1-build.1_DCG_Mac_Video_20230625_1129.zip"
 export IRIS_CDN_URL_WINDOWS="https://download.agora.io/sdk/release/iris_4.2.1-build.1_DCG_Windows_Video_20230625_1112.zip"
