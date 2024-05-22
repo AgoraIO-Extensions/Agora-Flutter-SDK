e5440f9d3257510ef76795f04a1d82a7ac810c09
littleGnAl <tearjeaker@hotmail.com>
2024-05-22 16:58:44 +0800

    [example][screen_sharing] Allow input the screen capture frame rate by user

--- example/lib/examples/advanced/screen_sharing/screen_sharing.dart
+++ example/lib/examples/advanced/screen_sharing/screen_sharing.dart
@@ -505,6 +505,8 @@ class _ScreenShareDesktopState extends State<ScreenShareDesktop>
   List<ScreenCaptureSourceInfo> _screenCaptureSourceInfos = [];
   late ScreenCaptureSourceInfo _selectedScreenCaptureSourceInfo;
 
+  late final TextEditingController _screenShareFrameRateController;
+
   @override
   bool get isScreenShared => widget.isScreenShared;
 
@@ -589,6 +591,8 @@ class _ScreenShareDesktopState extends State<ScreenShareDesktop>
   void initState() {
     super.initState();
 
+    _screenShareFrameRateController = TextEditingController(text: '30');
+
     _initScreenCaptureSourceInfos();
   }
 
@@ -599,6 +603,14 @@ class _ScreenShareDesktopState extends State<ScreenShareDesktop>
       mainAxisAlignment: MainAxisAlignment.start,
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
+        TextField(
+          controller: _screenShareFrameRateController,
+          decoration:
+              const InputDecoration(hintText: 'Screen Sharing frame rate'),
+        ),
+        const SizedBox(
+          height: 20,
+        ),
         _createDropdownButton(),
         if (_screenCaptureSourceInfos.isNotEmpty)
           Row(
@@ -629,18 +641,18 @@ class _ScreenShareDesktopState extends State<ScreenShareDesktop>
       await rtcEngine.startScreenCaptureByDisplayId(
           displayId: sourceId!,
           regionRect: const Rectangle(x: 0, y: 0, width: 0, height: 0),
-          captureParams: const ScreenCaptureParameters(
+          captureParams: ScreenCaptureParameters(
             captureMouseCursor: true,
-            frameRate: 30,
+            frameRate: int.parse(_screenShareFrameRateController.text),
           ));
     } else if (_selectedScreenCaptureSourceInfo.type ==
         ScreenCaptureSourceType.screencapturesourcetypeWindow) {
       await rtcEngine.startScreenCaptureByWindowId(
         windowId: sourceId!,
         regionRect: const Rectangle(x: 0, y: 0, width: 0, height: 0),
-        captureParams: const ScreenCaptureParameters(
+        captureParams: ScreenCaptureParameters(
           captureMouseCursor: true,
-          frameRate: 30,
+          frameRate: int.parse(_screenShareFrameRateController.text),
         ),
       );
     }
