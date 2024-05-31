27ab071007a846ecd3acead17467ea8dab2c79fd
Littlegnal <8847263+littleGnAl@users.noreply.github.com>
2024-05-31 17:15:20 +0800

    feat: upgrade native sdk dependencies 20240531 (#1802)
    Update native sdk dependencies 20240531
    native sdk dependencies:
    ```
    
    ```
    
    iris dependencies:
    ```
    Iris iOS: Artifactory: https://artifactory-api.bj2.agoralab.co/artifactory/CSDC_repo/EP/4.3/20240531/ios/iris_4.3.2-dev.6_DCG_iOS_Video_Standalone_20240531_0442_407.zip https://artifactory-api.bj2.agoralab.co/artifactory/CSDC_repo/EP/4.3/20240531/ios/iris_4.3.2-dev.6_DCG_iOS_Video_20240531_0442_407.zip PrivateArtifactory: https://artifactory-api.bj2.agoralab.co/artifactory/CSDC_repo/EP/4.3/20240531/ios/iris_4.3.2-dev.6_DCG_iOS_Video_20240531_0442_407_private.zip  CDN: https://download.agora.io/sdk/release/iris_4.3.2-dev.6_DCG_iOS_Video_Standalone_20240531_0442_407.zip https://download.agora.io/sdk/release/iris_4.3.2-dev.6_DCG_iOS_Video_20240531_0442_407.zip Cocoapods: pod 'AgoraIrisRTC_iOS', '4.3.2-dev.6'  Iris macOS: Artifactory: https://artifactory-api.bj2.agoralab.co/artifactory/CSDC_repo/EP/4.3/20240531/mac/iris_4.3.2-dev.6_DCG_Mac_Video_20240531_0442_403.zip https://artifactory-api.bj2.agoralab.co/artifactory/CSDC_repo/EP/4.3/20240531/mac/iris_4.3.2-dev.6_DCG_Mac_Video_Standalone_20240531_0442_403.zip PrivateArtifactory: https://artifactory-api.bj2.agoralab.co/artifactory/CSDC_repo/EP/4.3/20240531/mac/iris_4.3.2-dev.6_DCG_Mac_Video_20240531_0442_403_private.zip  CDN: https://download.agora.io/sdk/release/iris_4.3.2-dev.6_DCG_Mac_Video_20240531_0442_403.zip https://download.agora.io/sdk/release/iris_4.3.2-dev.6_DCG_Mac_Video_Standalone_20240531_0442_403.zip Cocoapods: pod 'AgoraIrisRTC_macOS', '4.3.2-dev.6'  Iris Android: Artifactory: https://artifactory-api.bj2.agoralab.co/artifactory/CSDC_repo/EP/4.3/20240531/android/iris_4.3.2-dev.6_DCG_Android_Video_Standalone_20240531_0442_502.zip https://artifactory-api.bj2.agoralab.co/artifactory/CSDC_repo/EP/4.3/20240531/android/iris_4.3.2-dev.6_DCG_Android_Video_20240531_0442_502.zip PrivateArtifactory: https://artifactory-api.bj2.agoralab.co/artifactory/CSDC_repo/EP/4.3/20240531/android/iris_4.3.2-dev.6_DCG_Android_Video_20240531_0442_502_private.zip  CDN: https://download.agora.io/sdk/release/iris_4.3.2-dev.6_DCG_Android_Video_Standalone_20240531_0442_502.zip https://download.agora.io/sdk/release/iris_4.3.2-dev.6_DCG_Android_Video_20240531_0442_502.zip Maven: implementation 'io.agora.rtc:iris-rtc:4.3.2-dev.6'  Iris Windows: Artifactory: https://artifactory-api.bj2.agoralab.co/artifactory/CSDC_repo/EP/4.3/20240531/windows/iris_4.3.2-dev.6_DCG_Windows_Video_20240531_0442_440.zip https://artifactory-api.bj2.agoralab.co/artifactory/CSDC_repo/EP/4.3/20240531/windows/iris_4.3.2-dev.6_DCG_Windows_Video_Standalone_20240531_0442_440.zip PrivateArtifactory: https://artifactory-api.bj2.agoralab.co/artifactory/CSDC_repo/EP/4.3/20240531/windows/iris_4.3.2-dev.6_DCG_Windows_Video_20240531_0442_440_private.zip  CDN: https://download.agora.io/sdk/release/iris_4.3.2-dev.6_DCG_Windows_Video_20240531_0442_440.zip https://download.agora.io/sdk/release/iris_4.3.2-dev.6_DCG_Windows_Video_Standalone_20240531_0442_440.zip
    ```
    
    > This pull request is trigger by bot, DO NOT MODIFY BY HAND.
    
    Co-authored-by: littleGnAl <littleGnAl@users.noreply.github.com>

--- scripts/artifacts_version.sh
+++ scripts/artifacts_version.sh
@@ -1,6 +1,6 @@
 set -e
 
-export IRIS_CDN_URL_ANDROID="https://download.agora.io/sdk/release/iris_4.3.2-dev.5_DCG_Android_Video_20240531_1116_501.zip"
-export IRIS_CDN_URL_IOS="https://download.agora.io/sdk/release/iris_4.3.2-dev.5_DCG_iOS_Video_20240531_1116_406.zip"
-export IRIS_CDN_URL_MACOS="https://download.agora.io/sdk/release/iris_4.3.2-dev.5_DCG_Mac_Video_20240531_1116_402.zip"
-export IRIS_CDN_URL_WINDOWS="https://download.agora.io/sdk/release/iris_4.3.2-dev.5_DCG_Windows_Video_20240531_1116_439.zip"
+export IRIS_CDN_URL_ANDROID="https://download.agora.io/sdk/release/iris_4.3.2-dev.6_DCG_Android_Video_20240531_0442_502.zip"
+export IRIS_CDN_URL_IOS="https://download.agora.io/sdk/release/iris_4.3.2-dev.6_DCG_iOS_Video_20240531_0442_407.zip"
+export IRIS_CDN_URL_MACOS="https://download.agora.io/sdk/release/iris_4.3.2-dev.6_DCG_Mac_Video_20240531_0442_403.zip"
+export IRIS_CDN_URL_WINDOWS="https://download.agora.io/sdk/release/iris_4.3.2-dev.6_DCG_Windows_Video_20240531_0442_440.zip"
