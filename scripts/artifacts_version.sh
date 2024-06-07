4442da05ad9ca308dac0dec990b4390917a07907
Littlegnal <8847263+littleGnAl@users.noreply.github.com>
2024-06-07 10:59:02 +0800

    feat: upgrade native sdk dependencies 20240607 (#1824)
    Update native sdk dependencies 20240607
    native sdk dependencies:
    ```
    
    ```
    
    iris dependencies:
    ```
    Iris: Artifactory: https://artifactory-api.bj2.agoralab.co/artifactory/CSDC_repo/IRIS_RELEASE/4.2.2.142-build.1/iris_4.2.2.142-build.1_DCG_Windows_Video_20240329_1152.zip Symbol: https://artifactory-api.bj2.agoralab.co/artifactory/CSDC_repo/IRIS_RELEASE/4.2.2.142-build.1/iris_4.2.2.142-build.1_DCG_Windows_Video_20240329_1152_symbols.zip CDN: https://download.agora.io/sdk/release/iris_4.2.2.142-build.1_DCG_Windows_Video_20240329_1152.zip
    ```
    
    > This pull request is trigger by bot, DO NOT MODIFY BY HAND.
    
    Co-authored-by: littleGnAl <littleGnAl@users.noreply.github.com>

--- scripts/artifacts_version.sh
+++ scripts/artifacts_version.sh
@@ -3,4 +3,4 @@ set -e
 export IRIS_CDN_URL_ANDROID="https://download.agora.io/sdk/release/iris_4.2.6.10-build.1_DCG_Android_Video_20240509_1139.zip"
 export IRIS_CDN_URL_IOS="https://download.agora.io/sdk/release/iris_4.2.6.10-build.1_DCG_iOS_Video_20240509_1139.zip"
 export IRIS_CDN_URL_MACOS="https://download.agora.io/sdk/release/iris_4.2.6.10-build.1_DCG_Mac_Video_20240509_1139.zip"
-export IRIS_CDN_URL_WINDOWS="https://download.agora.io/sdk/release/iris_4.2.6.10-build.1_DCG_Windows_Video_20240509_1139.zip"
+export IRIS_CDN_URL_WINDOWS="https://download.agora.io/sdk/release/iris_4.2.2.142-build.1_DCG_Windows_Video_20240329_1152.zip"
