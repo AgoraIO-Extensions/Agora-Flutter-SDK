22e066c98fb872f1ae408c98b4c3368c111ce296
littleGnAl <littleGnAl@users.noreply.github.com>
2024-06-24 03:24:21 +0000

    feat: upgrade native sdk dependencies 20240624

--- scripts/artifacts_version.sh
+++ scripts/artifacts_version.sh
@@ -1,6 +1,6 @@
 set -e
 
-export IRIS_CDN_URL_ANDROID="https://download.agora.io/sdk/release/iris_4.2.6.10-build.1_DCG_Android_Video_20240509_1139.zip"
-export IRIS_CDN_URL_IOS="https://download.agora.io/sdk/release/iris_4.2.6.10-build.1_DCG_iOS_Video_20240509_1139.zip"
-export IRIS_CDN_URL_MACOS="https://download.agora.io/sdk/release/iris_4.2.6.10-build.1_DCG_Mac_Video_20240509_1139.zip"
-export IRIS_CDN_URL_WINDOWS="https://download.agora.io/sdk/release/iris_4.2.6.14-dev.1_DCG_Windows_Video_20240619_0135.zip"
+export IRIS_CDN_URL_ANDROID="https://download.agora.io/sdk/release/iris_4.2.6.14-build.1_DCG_Android_Video_20240624_1012.zip"
+export IRIS_CDN_URL_IOS="https://download.agora.io/sdk/release/iris_4.2.6.14-build.1_DCG_iOS_Video_20240624_1016.zip"
+export IRIS_CDN_URL_MACOS="https://download.agora.io/sdk/release/iris_4.2.6.14-build.1_DCG_Mac_Video_20240624_1012.zip"
+export IRIS_CDN_URL_WINDOWS="https://download.agora.io/sdk/release/iris_4.2.6.14-build.1_DCG_Windows_Video_20240624_1012.zip"
