b82c5c006bbc903bc94d9b8b1c7407e206e7a809
gxz <guoxianzhe@agora.io>
2024-08-28 15:40:37 +0800

    chore: optimize

--- scripts/iris_web_version.js
+++ scripts/iris_web_version.js
@@ -1,8 +1,8 @@
 // Share the iris web url to all the tests
 
 // This url should be same as the url inside the `example/web/index.html`
-const irisWebUrl = 'https://github.com/AgoraIO-Extensions/iris_web/releases/download/v0.8.0-pre/iris-web-rtc_n440_w4220_0.8.0-pre.js?v=1';
-const irisWebFakeUrl = 'https://github.com/AgoraIO-Extensions/iris_web/releases/download/v0.8.0-pre/iris-web-rtc-fake_n440_w4220_0.8.0-pre.js?v=1';
+const irisWebUrl = 'https://github.com/AgoraIO-Extensions/iris_web/releases/download/v0.8.0-pre/iris-web-rtc_n440_w4220_0.8.0-pre1.js';
+const irisWebFakeUrl = 'https://github.com/AgoraIO-Extensions/iris_web/releases/download/v0.8.0-pre/iris-web-rtc-fake_n440_w4220_0.8.0-pre1.js';
 
 (function() {
     var scriptLoaded = false;
