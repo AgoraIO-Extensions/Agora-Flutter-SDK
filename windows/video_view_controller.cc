0edbfe5e71863fc34f2d2de00254776cf59a3be9
Littlegnal <8847263+littleGnAl@users.noreply.github.com>
2024-09-06 16:32:55 +0800

    fix: [windows] Fix TextureRender crash (#1999)
    We can't delete the `TextureRender` in the
    `VideoViewController::DestroyTextureRender`, because the
    `flutter::TextureRegistrar::UnregisterTexture` is called in the Flutter
    rasterizer thread, it's not thread-safe.
    
    We change the `VideoViewController::renderers_` to `std::map<int64_t,
    TextureRender *>`, which holds the raw pointer `TextureRender *`, and
    delete the `TextureRender ` in the
    `flutter::TextureRegistrar::UnregisterTexture` callback which introduces
    from Flutter SDK 3.7.0.

--- windows/video_view_controller.cc
+++ windows/video_view_controller.cc
@@ -41,6 +41,7 @@ VideoViewController::VideoViewController(
 
 VideoViewController::~VideoViewController()
 {
+  Dispose();
 }
 
 void VideoViewController::HandleMethodCall(
@@ -154,15 +155,16 @@ int64_t VideoViewController::CreateTextureRender(
     unsigned int videoViewSetupMode)
 {
   agora::iris::IrisRtcRendering *iris_rtc_rendering = reinterpret_cast<agora::iris::IrisRtcRendering *>(irisRtcRenderingHandle);
-  std::unique_ptr<TextureRender> textureRender = std::make_unique<TextureRender>(
+  auto textureRender = new TextureRender(
       messenger_,
       texture_registrar_,
       iris_rtc_rendering);
+
   int64_t texture_id = textureRender->texture_id();
 
-  textureRender.get()->UpdateData(uid, channelId, videoSourceType, videoViewSetupMode);
+  textureRender->UpdateData(uid, channelId, videoSourceType, videoViewSetupMode);
 
-  renderers_[texture_id] = std::move(textureRender);
+  renderers_[texture_id] = textureRender;
   return texture_id;
 }
 
