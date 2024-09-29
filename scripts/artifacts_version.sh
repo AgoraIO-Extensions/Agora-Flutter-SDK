f24eaf459052975c0c61de4b8cee5382a4ce9259
Littlegnal <8847263+littleGnAl@users.noreply.github.com>
2024-09-29 18:18:20 +0800

    feat: upgrade native sdk dependencies 20240929 (#2040)
    Update native dependencies 20240929
    native dependencies:
    ```
    Iris iOS: Artifactory: https://artifactory-api.bj2.agoralab.co/artifactory/CSDC_repo/EP/4.5/20240929/ios/iris_4.5.0-dev.5_DCG_iOS_Video_20240929_0602_538.zip https://artifactory-api.bj2.agoralab.co/artifactory/CSDC_repo/EP/4.5/20240929/ios/iris_4.5.0-dev.5_DCG_iOS_Video_Standalone_20240929_0602_538.zip PrivateArtifactory: https://artifactory-api.bj2.agoralab.co/artifactory/CSDC_repo/EP/4.5/20240929/ios/iris_4.5.0-dev.5_DCG_iOS_Video_20240929_0602_538_xdump.zip https://artifactory-api.bj2.agoralab.co/artifactory/CSDC_repo/EP/4.5/20240929/ios/iris_4.5.0-dev.5_DCG_iOS_Video_20240929_0602_538_private.zip  CDN: https://download.agora.io/sdk/release/iris_4.5.0-dev.5_DCG_iOS_Video_20240929_0602_538.zip https://download.agora.io/sdk/release/iris_4.5.0-dev.5_DCG_iOS_Video_Standalone_20240929_0602_538.zip Cocoapods: pod 'AgoraIrisRTC_iOS', '4.5.0-dev.5'
    ```
    
    > This pull request is trigger by bot, DO NOT MODIFY BY HAND.
    
    Co-authored-by: littleGnAl <littleGnAl@users.noreply.github.com>

--- scripts/artifacts_version.sh
+++ scripts/artifacts_version.sh
@@ -1,6 +1,6 @@
 set -e
 
 export IRIS_CDN_URL_ANDROID="https://download.agora.io/sdk/release/iris_4.5.0-dev.4_DCG_Android_Video_16K_20240927_0549_660.zip"
-export IRIS_CDN_URL_IOS="https://download.agora.io/sdk/release/iris_4.5.0-dev.4_DCG_iOS_Video_20240927_0552_535.zip"
+export IRIS_CDN_URL_IOS="https://download.agora.io/sdk/release/iris_4.5.0-dev.5_DCG_iOS_Video_20240929_0602_538.zip"
 export IRIS_CDN_URL_MACOS="https://download.agora.io/sdk/release/iris_4.5.0-dev.4_DCG_Mac_Video_20240927_0549_505.zip"
 export IRIS_CDN_URL_WINDOWS="https://download.agora.io/sdk/release/iris_4.5.0-dev.4_DCG_Windows_Video_20240927_0549_544.zip"
