1bf25539f314b27f174b3b1ce5dbfcc3ef3bec49
sylar <peilinok@163.com>
2024-11-21 00:00:11 +0800

    feat: fix missed cdn urls

--- scripts/artifacts_version.sh
+++ scripts/artifacts_version.sh
@@ -1,6 +1,6 @@
 set -e
 
-export IRIS_CDN_URL_ANDROID="https://download.agora.io/sdk/release/iris_4.3.1.136-build.1_DCG_Android_Video_20240722_0455_555.zip"
-export IRIS_CDN_URL_IOS="https://download.agora.io/sdk/release/iris_4.3.1.136-build.1_DCG_iOS_Video_20240722_0527_457.zip"
+export IRIS_CDN_URL_ANDROID="https://download.agora.io/sdk/release/iris_4.3.1.201-build.1_DCG_Android_Video_20241120_0224_694.zip"
+export IRIS_CDN_URL_IOS="https://download.agora.io/sdk/release/iris_4.3.1.201-build.1_DCG_iOS_Video_20241120_0226_568.zip"
 export IRIS_CDN_URL_MACOS="https://download.agora.io/sdk/release/iris_4.3.1.201-build.1_DCG_Mac_Video_20241120_0224_528.zip"
 export IRIS_CDN_URL_WINDOWS="https://download.agora.io/sdk/release/iris_4.3.1.201-build.1_DCG_Windows_Video_20241120_0431_571.zip"
