01199199bfcdb83496e03a2472cc1aca339a9163
peilinok <peilinok@users.noreply.github.com>
2024-11-22 05:59:08 +0000

    feat: upgrade native sdk 4.2.6.147

--- scripts/artifacts_version.sh
+++ scripts/artifacts_version.sh
@@ -3,4 +3,4 @@ set -e
 export IRIS_CDN_URL_ANDROID="https://download.agora.io/sdk/release/iris_4.2.6.142-build.1_DCG_Android_Video_20240815_0148.zip"
 export IRIS_CDN_URL_IOS="https://download.agora.io/sdk/release/iris_4.2.6.142-build.1_DCG_iOS_Video_20240815_0148.zip"
 export IRIS_CDN_URL_MACOS="https://download.agora.io/sdk/release/iris_4.2.6.142-build.1_DCG_Mac_Video_20240815_0148.zip"
-export IRIS_CDN_URL_WINDOWS="https://download.agora.io/sdk/release/iris_4.2.6.142-build.1_DCG_Windows_Video_20240815_0148.zip"
+export IRIS_CDN_URL_WINDOWS="https://download.agora.io/sdk/release/iris_4.2.6.147-build.1_DCG_Windows_Video_20241122_1154.zip"
