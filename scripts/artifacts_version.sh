6b425c087ada3e253acddf61e19360c33085b149
peilinok <peilinok@users.noreply.github.com>
2024-12-12 07:31:03 +0000

    feat: upgrade native audio sdk 4.4.0

--- scripts/artifacts_version.sh
+++ scripts/artifacts_version.sh
@@ -1,6 +1,6 @@
 set -e
 
-export IRIS_CDN_URL_ANDROID="https://download.agora.io/sdk/release/iris_4.4.0-build.6_DCG_Android_Video_20240823_1146_612.zip"
-export IRIS_CDN_URL_IOS="https://download.agora.io/sdk/release/iris_4.4.0-build.6_DCG_iOS_Video_20240823_1148_496.zip"
+export IRIS_CDN_URL_ANDROID="https://download.agora.io/sdk/release/iris_4.4.0-build.7_DCG_Android_Audio_20241212_0255_708.zip"
+export IRIS_CDN_URL_IOS="https://download.agora.io/sdk/release/iris_4.4.0-build.7_DCG_iOS_Audio_20241212_0255_583.zip"
 export IRIS_CDN_URL_MACOS="https://download.agora.io/sdk/release/iris_4.4.0-build.6_DCG_Mac_Video_20240823_1146_468.zip"
 export IRIS_CDN_URL_WINDOWS="https://download.agora.io/sdk/release/iris_4.4.0-build.6_DCG_Windows_Video_20240823_1146_504.zip"
