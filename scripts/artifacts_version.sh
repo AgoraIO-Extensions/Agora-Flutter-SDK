f005453831bde30eb9b1a4a1d635bc4829158098
sylar <peilinok@163.com>
2025-01-15 10:41:44 +0800

    feat: upgrade iris to 4.3.2.234-build.3

--- scripts/artifacts_version.sh
+++ scripts/artifacts_version.sh
@@ -1,6 +1,6 @@
 set -e
 
-export IRIS_CDN_URL_ANDROID="https://download.agora.io/sdk/release/iris_4.3.2.234-build.1_DCG_Android_Video_20241218_1023_711.zip"
-export IRIS_CDN_URL_IOS="https://download.agora.io/sdk/release/iris_4.3.2.234-build.1_DCG_iOS_Video_20241218_1025_587.zip"
+export IRIS_CDN_URL_ANDROID="https://download.agora.io/sdk/release/iris_4.3.2.234-build.3_DCG_Android_Video_20250115_1019_721.zip"
+export IRIS_CDN_URL_IOS="https://download.agora.io/sdk/release/iris_4.3.2.234-build.3_DCG_iOS_Video_20250115_1019_600.zip"
 export IRIS_CDN_URL_MACOS="https://download.agora.io/sdk/release/iris_4.3.2.11-build.1_DCG_Mac_Video_20241206_1145_537.zip"
 export IRIS_CDN_URL_WINDOWS="https://download.agora.io/sdk/release/iris_4.3.2.11-build.1_DCG_Windows_Video_20241206_1146_579.zip"
