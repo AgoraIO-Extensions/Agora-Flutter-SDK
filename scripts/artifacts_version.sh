8901f688f8df7b9a328c2caa6d40ff7c4042dc8b
Sylar <peilinok@163.com>
2025-01-17 14:38:06 +0800

    chore: release 6.3.2-sp.43211.b.3 (#2183)

--- scripts/artifacts_version.sh
+++ scripts/artifacts_version.sh
@@ -1,6 +1,6 @@
 set -e
 
 export IRIS_CDN_URL_ANDROID="https://download.agora.io/sdk/release/iris_4.3.2.11-build.1_DCG_Android_Video_20241206_1145_704.zip"
-export IRIS_CDN_URL_IOS="https://download.agora.io/sdk/release/iris_4.3.2.11-build.1_DCG_iOS_Video_20241206_1148_579.zip"
+export IRIS_CDN_URL_IOS="https://download.agora.io/sdk/release/iris_4.3.2.11-build.3_DCG_iOS_Video_20250116_0426_602.zip"
 export IRIS_CDN_URL_MACOS="https://download.agora.io/sdk/release/iris_4.3.2.11-build.1_DCG_Mac_Video_20241206_1145_537.zip"
 export IRIS_CDN_URL_WINDOWS="https://download.agora.io/sdk/release/iris_4.3.2.11-build.1_DCG_Windows_Video_20241206_1146_579.zip"
