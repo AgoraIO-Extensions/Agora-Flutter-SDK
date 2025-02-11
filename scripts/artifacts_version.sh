b7fb9371b09b54ba0efcdea55fac85c692f3d265
sda-rob <149643938+sda-rob@users.noreply.github.com>
2025-02-11 15:57:42 +0800

    feat: upgrade native sdk 4.5.0 (#2195)
    Update native sdk 4.5.0 dependencies
    
    > This pull request is trigger by bot, you can checkout this branch and
    update it.
    
    Co-authored-by: peilinok <peilinok@users.noreply.github.com>

--- scripts/artifacts_version.sh
+++ scripts/artifacts_version.sh
@@ -1,6 +1,6 @@
 set -e
 
-export IRIS_CDN_URL_ANDROID="https://download.agora.io/sdk/release/iris_4.5.0-build.1_DCG_Android_Video_16K_20241203_0322_701.zip"
-export IRIS_CDN_URL_IOS="https://download.agora.io/sdk/release/iris_4.5.0-build.1_DCG_iOS_Video_20241203_0325_575.zip"
+export IRIS_CDN_URL_ANDROID="https://download.agora.io/sdk/release/iris_4.5.0-build.4_DCG_Android_Audio_16K_20250210_0211_730.zip"
+export IRIS_CDN_URL_IOS="https://download.agora.io/sdk/release/iris_4.5.0-build.3_DCG_iOS_Audio_20250210_1043_608.zip"
 export IRIS_CDN_URL_MACOS="https://download.agora.io/sdk/release/iris_4.5.0-build.1_DCG_Mac_Video_20241203_0322_534.zip"
 export IRIS_CDN_URL_WINDOWS="https://download.agora.io/sdk/release/iris_4.5.0-build.1_DCG_Windows_Video_20241203_0322_577.zip"
