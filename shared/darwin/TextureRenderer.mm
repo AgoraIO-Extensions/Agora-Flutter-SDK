633e1c6400ca4a3f5378854fda0953e403b7cf7a
Sylar <peilinok@163.com>
2025-04-29 10:23:07 +0800

    fix: copy CVPixelBuffer for macOS (#2298)

--- shared/darwin/TextureRenderer.mm
+++ shared/darwin/TextureRenderer.mm
@@ -1,4 +1,7 @@
 #import "TextureRenderer.h"
+#if defined(TARGET_OS_OSX) && TARGET_OS_OSX
+#import "AgoraCVPixelBufferUtils.h"
+#endif
 
 #import <AgoraRtcKit/AgoraMediaBase.h>
 #import <AgoraRtcKit/IAgoraMediaEngine.h>
@@ -95,10 +98,19 @@ public:
     // properties.
     dispatch_sync(strongRenderer.pixelBufferSynchronizationQueue, ^{
       previousPixelBuffer = strongRenderer.latestPixelBuffer;
+      // There has been a bug since RTC 4.4.0 that the pixel buffer ref count is not updated correctly,
+      // which will cause the flutter engine copy the wrong pixel buffer to the skia texture.
+      // So we need to copy the pixel buffer directly to work around this issue for now, after the issue is fixed,
+      // we can revert to the original code.
+#if defined(TARGET_OS_OSX) && TARGET_OS_OSX
+      strongRenderer.latestPixelBuffer =
+          [AgoraCVPixelBufferUtils copyCVPixelBuffer:pixelBuffer];
+#else
       strongRenderer.latestPixelBuffer = CVPixelBufferRetain(pixelBuffer);
+#endif
     });
     if (previousPixelBuffer) {
-      CFRelease(previousPixelBuffer);
+      CVPixelBufferRelease(previousPixelBuffer);
     }
 
     // notify new frame available on main thread
