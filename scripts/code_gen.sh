ca8790dbea7e1296011a57484df48f137a0e3761
Gopein <z_gaopeng@sina.com>
2025-10-22 14:26:16 +0800

    chore: upgrade workflows

--- scripts/code_gen.sh
+++ scripts/code_gen.sh
@@ -2,15 +2,22 @@
 set -e
 set -x
 
-TERRA_PATH=$1
+RTC_VERSION=$1
 MY_PATH=$(realpath $(dirname "$0"))
 PROJECT_ROOT=$(realpath ${MY_PATH}/..)
 
 pushd ${PROJECT_ROOT}
 
+flutter --version
+
 flutter packages get
-bash ${PROJECT_ROOT}/tool/terra/build.sh ${TERRA_PATH}
+
+bash ${PROJECT_ROOT}/tool/terra/build.sh ${RTC_VERSION}
+
 bash ${MY_PATH}/flutter-build-runner.sh
+
 bash ${PROJECT_ROOT}/tool/testcase_gen/build.sh
+
+dart format .
 
 popd
--- tool/terra/build.sh
+++ tool/terra/build.sh
@@ -2,16 +2,31 @@
 set -e
 set -x
 
+NEW_VERSION=$1
 MY_PATH=$(realpath $(dirname "$0"))
 PROJECT_ROOT=$(realpath ${MY_PATH}/../..)
+
+TERRA_MAIN_FILE=${PROJECT_ROOT}/tool/terra/terra_config_main.yaml
+
+if [ -n "$NEW_VERSION" ]; then
+  # Check the operating system type
+  if [[ "$OSTYPE" == "darwin"* ]]; then
+    # macOS
+    sed -i '' -E "s/rtc_[0-9]+\.[0-9]+(\.[0-9]+)*/${NEW_VERSION}/g" $TERRA_MAIN_FILE
+  else
+    # Linux and other Unix-like systems
+    sed -i -E "s/rtc_[0-9]+\.[0-9]+(\.[0-9]+)*/${NEW_VERSION}/g" $TERRA_MAIN_FILE
+  fi
+  echo "Updated version to ${NEW_VERSION} in $TERRA_MAIN_FILE"
+fi
 
 bash ${MY_PATH}/prepare.sh
 
 pushd ${MY_PATH}
 
 npm exec terra -- run \
-    --config ${PROJECT_ROOT}/tool/terra/terra_config_main.yaml  \
-    --output-dir=${PROJECT_ROOT}/
+    --config ${TERRA_MAIN_FILE}  \
+    --output-dir=${PROJECT_ROOT}
 
 popd
 
