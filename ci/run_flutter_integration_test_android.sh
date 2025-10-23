202f7607e7e960209fa1f42228df1164d83afdbb
小瓶子 <z_gaopeng@sina.com>
2025-10-23 11:10:10 +0800

    upgrade workflow (#2464)

--- ci/run_flutter_integration_test_android.sh
+++ ci/run_flutter_integration_test_android.sh
@@ -5,22 +5,6 @@ set -x
 
 MY_PATH=$(dirname "$0")
 
-DOWNLOAD_IRIS_DEBUGGER=${1:-1}
-
-if [[ ${DOWNLOAD_IRIS_DEBUGGER} == 1 ]];then
-    source ${MY_PATH}/../scripts/artifacts_version.sh
-
-    bash ${MY_PATH}/../scripts/download_unzip_iris_cdn_artifacts.sh ${IRIS_CDN_URL_ANDROID} "Android"
-fi
-
-pushd ${MY_PATH}/../test_shard/fake_test_app
-
-flutter packages get
-
-flutter test integration_test --verbose
-
-popd
-
 pushd ${MY_PATH}/../test_shard/integration_test_app
 
 flutter packages get
