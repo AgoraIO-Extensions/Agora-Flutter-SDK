77e443a80c2691c7c0917361ffab1c3824d3012b
Gopein <z_gaopeng@sina.com>
2025-11-04 16:05:13 +0800

    chore: add workflow

---
+++ ci/run_flutter_integration_test_web.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+
+MY_PATH=$(realpath $(dirname "$0"))
+PROJECT_ROOT=$(realpath ${MY_PATH}/..)
+
+pushd ${PROJECT_ROOT}/test_shard/fake_test_app
+
+IRIS_WEB_VERSION_PATH=${PROJECT_ROOT}/scripts/iris_web_version.js
+rm -rf web/iris_web_version.js
+cp -RP ${IRIS_WEB_VERSION_PATH} web/
+
+echo "Run integration test on web"
+echo "If you want to run integration test on your local machine, please follow the https://docs.flutter.dev/testing/integration-tests#test-in-a-web-browser to setup first."
+
+flutter packages get
+
+for filename in integration_test/*.dart; do
+    if [[ "$filename" == *.generated.dart  ]]; then
+        continue
+    fi
+
+    flutter drive \
+        --verbose-system-logs \
+        -d web-server \
+        --driver=test_driver/integration_test.dart \
+        --target=${filename}
+done
+
+popd
--- scripts/artifacts_version.sh
+++
@@ -1,6 +0,0 @@
-set -e
-
-export IRIS_CDN_URL_ANDROID="https://download.agora.io/sdk/release/iris_4.2.6.20-build.1_DCG_Android_Video_20241217_0416.zip"
-export IRIS_CDN_URL_IOS="https://download.agora.io/sdk/release/iris_4.2.6.20-build.1_DCG_iOS_Video_20241217_0419.zip"
-export IRIS_CDN_URL_MACOS="https://download.agora.io/sdk/release/iris_4.2.6.20-build.1_DCG_Mac_Video_20241217_0416.zip"
-export IRIS_CDN_URL_WINDOWS="https://download.agora.io/sdk/release/iris_4.2.6.20-build.1_DCG_Windows_Video_20241217_0416.zip"
