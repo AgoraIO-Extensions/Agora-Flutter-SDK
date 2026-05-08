c45670dcf89d6a24bf7f8d72d8fb68fb4e8e225a
Gopein <z_gaopeng@sina.com>
2026-05-08 21:24:20 +0800

    chore: update package manager and configure approved Git repositories

--- tool/terra/prepare.sh
+++ tool/terra/prepare.sh
@@ -12,6 +12,14 @@ rm -rf yarn.lock
 echo "nodeLinker: node-modules" >> .yarnrc.yml
 echo "enableImmutableInstalls: false" >> .yarnrc.yml
 yarn set version berry
+
+# Yarn 4.14+ blocks git: protocol deps unless explicitly allowed (YN0080).
+cat >> .yarnrc.yml << 'EOF'
+
+approvedGitRepositories:
+  - "ssh://git@github.com/AgoraIO-Extensions/*"
+EOF
+
 yarn
 
 popd
