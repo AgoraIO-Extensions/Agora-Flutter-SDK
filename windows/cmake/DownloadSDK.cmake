515e7fde50722172a9da6aa86829fdd0d4e7559a
Gopein <z_gaopeng@sina.com>
2026-05-11 14:35:09 +0800

    fix: downgrade iris dependencies to version 4.5.2.8-build.1 across Android, iOS, macOS, and Windows

--- windows/cmake/DownloadSDK.cmake
+++ windows/cmake/DownloadSDK.cmake
@@ -1,5 +1,5 @@
 # iris dependencies start
-set(IRIS_SDK_DOWNLOAD_URL "https://download.agora.io/sdk/release/iris_4.5.2.9-build.1_DCG_Windows_Video_Standalone_20260508_0833_31968.zip")
+set(IRIS_SDK_DOWNLOAD_URL "https://download.agora.io/sdk/release/iris_4.5.2.8-build.1_DCG_Windows_Video_Standalone_20260112_0211_31902.zip")
 # iris dependencies end
 
 # native dependencies start
