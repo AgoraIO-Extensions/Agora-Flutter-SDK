df3fe8e4b5864997387418a91bfc8cd37fa84664
Alek Åström <alek.astrom@gmail.com>
2026-06-24 21:21:56 +0200

    Use exact Agora native SPM versions

--- macos/agora_rtc_engine/Package.swift
+++ macos/agora_rtc_engine/Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         .library(name: "agora-rtc-engine", targets: ["agora_rtc_engine"])
     ],
     dependencies: [
-        .package(url: "https://github.com/AgoraIO/AgoraRtcEngine_macOS.git", .upToNextMajor(from: "4.6.2")),
+        .package(url: "https://github.com/AgoraIO/AgoraRtcEngine_macOS.git", exact: "4.6.2"),
     ],
     targets: [
         .target(
