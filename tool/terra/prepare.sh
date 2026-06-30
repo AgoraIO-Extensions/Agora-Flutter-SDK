76f10b133bc678f1c497c24eb3c4554e6b9bf6b0
zhugaopeng <z_gaopeng@sina.com>
2026-06-30 17:45:01 +0800

    fix: unblock release dependency update workflow

--- tool/terra/prepare.sh
+++ tool/terra/prepare.sh
@@ -12,6 +12,11 @@ rm -rf yarn.lock
 echo "nodeLinker: node-modules" >> .yarnrc.yml
 echo "enableImmutableInstalls: false" >> .yarnrc.yml
 yarn set version berry
+cat >> .yarnrc.yml <<'EOF'
+approvedGitRepositories:
+  - "ssh://git@github.com/AgoraIO-Extensions/terra.git"
+  - "ssh://git@github.com/AgoraIO-Extensions/terra_shared_configs.git"
+EOF
 yarn
 
 popd
