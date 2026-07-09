6b69e53ea65d4267e456cab31f1d0fa51fb06c9e
sda-rob <sda-rob@users.noreply.github.com>
2026-07-09 09:37:17 +0000

    feat: update dependencies

--- windows/cmake/DownloadSDK.cmake
+++ windows/cmake/DownloadSDK.cmake
@@ -1,5 +1,5 @@
 # iris dependencies start
-set(IRIS_SDK_DOWNLOAD_URL "https://download.agora.io/sdk/release/iris_4.5.2.9.lite-build.1_DCG_Windows_Video_Standalone_20260310_0656_31936.zip")
+set(IRIS_SDK_DOWNLOAD_URL "https://download.agora.io/sdk/release/iris_4.5.2.9.lite-build.2_DCG_Windows_Video_Standalone_20260709_0451_31990.zip")
 # iris dependencies end
 
 # native dependencies start
