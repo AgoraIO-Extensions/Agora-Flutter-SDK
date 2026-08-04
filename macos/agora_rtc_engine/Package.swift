0ef455ab4b62a75bdc806e608a442274a5643d9e
ZGaopeng <ZGaopeng@users.noreply.github.com>
2026-08-04 08:20:57 +0000

    feat: update dependencies

--- macos/agora_rtc_engine/Package.swift
+++ macos/agora_rtc_engine/Package.swift
@@ -12,26 +12,30 @@ let package = Package(
         .library(name: "agora-rtc-engine", targets: ["agora_rtc_engine"])
     ],
     dependencies: [
-        .package(url: "https://github.com/AgoraIO/AgoraRtcEngine_macOS.git", .upToNextMajor(from: "4.5.2")),
+        // agora-spm-updater:managed-packages-start
+        .package(name: "FlutterFramework", path: "../FlutterFramework"),
+        .package(url: "https://github.com/AgoraIO/AgoraRtcEngine_macOS.git", exact: "4.6.2"),
+        // agora-spm-updater:managed-packages-end
     ],
     targets: [
         .target(
             name: "agora_rtc_engine",
             dependencies: [
+                // agora-spm-updater:managed-products-start
+                .product(name: "FlutterFramework", package: "FlutterFramework"),
                 .product(name: "RtcBasic", package: "AgoraRtcEngine_macOS"),
+                // agora-spm-updater:managed-products-end
                 "AgoraRtcWrapper"
             ],
             cSettings: [
                 .headerSearchPath("include/agora_rtc_engine")
-            ],
-            cxxSettings: [
-                .unsafeFlags(["-std=c++14"])
             ]
         ),
         .binaryTarget(
             name: "AgoraRtcWrapper",
-            url: "https://download.agora.io/sdk/release/AgoraIrisRTC_macOS-4.5.2-build.2.zip",
-            checksum: "2993bdaaa96a3e41a49cc830ccf98d58d099d3beb14f5d0c820540c0619049a7"
+            url: "https://download.agora.io/sdk/release/AgoraIrisRTC_macOS2-4.6.2-build.1.zip",
+            checksum: "dbfe2db86b0cb2c1012202212248bd6588173020c357dc13fc5a6dcf0a7b97cf"
         )
-    ]
+    ],
+    cxxLanguageStandard: .cxx14
 )
