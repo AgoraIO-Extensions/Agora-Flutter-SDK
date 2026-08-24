9c759b15bce35c149de9d071b7ce8af692bd3e69
zhugaopeng <z_gaopeng@sina.com>
2026-08-24 14:45:44 +0800

    ci: run Flutter 3.47 macOS tests with driver

--- ci/rendering_test_macos.sh
+++ ci/rendering_test_macos.sh
@@ -9,6 +9,15 @@ pushd ${MY_PATH}/../test_shard/rendering_test
 
 flutter packages get
 
-flutter test integration_test/agora_video_view_render_test.dart --dart-define=TEST_APP_ID="${TEST_APP_ID}" -d macos
+# flutter/flutter#189192 can leave `flutter test` waiting on desktop logs.
+if [[ "${FLUTTER_VERSION:-}" == "3.47.1" ]]; then
+    flutter drive -d macos \
+        --driver=test_driver/integration_test.dart \
+        --target=integration_test/agora_video_view_render_test.dart \
+        --dart-define=TEST_APP_ID="${TEST_APP_ID}"
+else
+    flutter test integration_test/agora_video_view_render_test.dart \
+        --dart-define=TEST_APP_ID="${TEST_APP_ID}" -d macos
+fi
 
 popd
