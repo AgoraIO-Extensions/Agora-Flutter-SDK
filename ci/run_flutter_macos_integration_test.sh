5a23e4179359679bc32d60a298631720f9c88b72
zhugaopeng <z_gaopeng@sina.com>
2026-08-24 10:59:28 +0800

    ci: pin latest Flutter macOS tests to macos-14

--- ci/run_flutter_macos_integration_test.sh
+++ ci/run_flutter_macos_integration_test.sh
@@ -9,18 +9,13 @@ pushd ${MY_PATH}/../test_shard/integration_test_app
 
 flutter packages get
 
-flutter_test_args=()
-if flutter test -v --help | grep -Fq -- '--[no-]enable-impeller'; then
-    flutter_test_args+=(--no-enable-impeller)
-fi
-
 # It's a little tricky that you should run integration test one by one on flutter macOS/Windows
 for filename in integration_test/*.dart; do
     if [[ "$filename" == *.generated.dart  ]]; then
         continue
     fi
     
-    flutter test "${flutter_test_args[@]}" "$filename" --dart-define=TEST_APP_ID="${TEST_APP_ID}" -d macos
+    flutter test $filename --dart-define=TEST_APP_ID="${TEST_APP_ID}" -d macos
 done
 
 popd
